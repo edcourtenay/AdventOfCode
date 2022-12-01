a19adde90495e6ed405b2e7546333233adc6ac93
Ed Courtenay <ed@edcourtenay.co.uk>
2022-12-01 21:33:59 +0000

    Add 2021 Day 06

---
+++ tests/AdventOfCode.Tests/Year2021/Day06Tests.cs
@@ -0,0 +1,29 @@
+﻿using AdventOfCode.Year2021;
+
+using FluentAssertions;
+
+namespace AdventOfCode.Tests.Year2021;
+
+public class Day06Tests : IClassFixture<Day06>
+{
+    private readonly Day06 _sut;
+
+    private const string TestData = "3,4,3,1,2";
+
+    public Day06Tests(Day06 sut)
+    {
+        _sut = sut;
+    }
+
+    [Fact(DisplayName = "Part1 should return expected results from example data")]
+    public void Part1Example()
+    {
+        _sut.Part1(TestData).As<long>().Should().Be(5934);
+    }
+
+    [Fact(DisplayName = "Part2 should return expected results from example data")]
+    public void Part2Example()
+    {
+        _sut.Part2(TestData).As<long>().Should().Be(26984457539);
+    }
+}
