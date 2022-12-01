27662b74e13a21ad1ceee8c765ea29aa835e45b6
Ed Courtenay <ed@edcourtenay.co.uk>
2022-12-01 22:41:34 +0000

    Add 2021 Day 08

---
+++ src/AdventOfCode/Year2021/Day08.cs
@@ -0,0 +1,47 @@
+﻿namespace AdventOfCode.Year2021;
+
+[Description("Seven Segment Search")]
+public class Day08 : IPuzzle
+{
+    public object Part1(string input)
+    {
+        return ExecutePart1(input.ToLines());
+    }
+
+    public object Part2(string input)
+    {
+        return string.Empty;
+    }
+
+    private readonly string[] Digits = {
+        "abcefg", //0
+        "cf", //1
+        "acdeg", //2
+        "acdfg", //3
+        "bcdf", //4
+        "abdfg", //5
+        "abdefg", //6
+        "acf", //7
+        "abcdefg", //8
+        "abcdfg"  //9
+    };
+
+    public long ExecutePart1(IEnumerable<string> data)
+    {
+        var lengths = new[] { 2, 4, 3, 7 };
+
+        var lines = data.Select(ParseLine);
+
+        return lines
+            .SelectMany(tuple => tuple.digit)
+            .Count(s => lengths.Contains(s.Length));
+    }
+
+    public (string[] observation, string[] digit) ParseLine(string line)
+    {
+        var split = line.Split('|', StringSplitOptions.RemoveEmptyEntries);
+        var observation = split[0].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        var digit = split[1].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        return (observation, digit);
+    }
+}
---
+++ tests/AdventOfCode.Tests/Year2021/Day08Tests.cs
@@ -0,0 +1,40 @@
+﻿using AdventOfCode.Year2021;
+
+using FluentAssertions;
+
+namespace AdventOfCode.Tests.Year2021;
+
+public class Day08Tests : IClassFixture<Day08>
+{
+    private readonly Day08 _sut;
+
+    private const string TestData = """
+    be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+    edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+    fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+    fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+    aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+    fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+    dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+    bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+    egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+    gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
+    """;
+
+    public Day08Tests(Day08 sut)
+    {
+        _sut = sut;
+    }
+
+    [Fact(DisplayName = "Part1 should return expected results from example data")]
+    public void Part1Example()
+    {
+        _sut.Part1(TestData).As<long>().Should().Be(26);
+    }
+
+    [Fact(DisplayName = "Part2 should return expected results from example data")]
+    public void Part2Example()
+    {
+        _sut.Part2(TestData).As<long>().Should().Be(61229);
+    }
+}
