1b28042113371a87d59ff326ff7b015c7c91d144
Ed Courtenay <ed@edcourtenay.co.uk>
2022-12-01 22:41:15 +0000

    Add 2021 Day 07

---
+++ src/AdventOfCode/Year2021/Day07.cs
@@ -0,0 +1,49 @@
+﻿namespace AdventOfCode.Year2021;
+
+[Description("The Treachery of Whales")]
+public class Day07 : IPuzzle
+{
+    public object Part1(string input)
+    {
+        (int result1, _) = Execute(input);
+        return result1;
+    }
+
+    public object Part2(string input)
+    {
+        (_, int result2) = Execute(input);
+        return result2;
+    }
+
+    private (int result1, int result2) Execute(string input)
+    {
+        var crabs = input.Split(',').Select(s => Convert.ToInt32(s)).ToArray();
+
+        var g = crabs.GroupBy(i => i).ToArray();
+        var min = crabs.Min();
+        var max = crabs.Max();
+
+        (int result1, int result2) = (Int32.MaxValue, Int32.MaxValue);
+
+        for (int i = min; i <= max; i++)
+        {
+            var (fuel1, fuel2) = SolveFor(g, i);
+
+            result1 = fuel1 < result1 ? fuel1 : result1;
+            result2 = fuel2 < result2 ? fuel2 : result2;
+        }
+
+        return (result1, result2);
+    }
+
+    private (int, int) SolveFor(IGrouping<int, int>[] crabs, int i) =>
+        (SolveFor1(crabs, i), SolveFor2(crabs, i));
+
+    private int SolveFor1(IGrouping<int,int>[] crabs, int i) =>
+        crabs.Sum(grouping => Math.Abs(grouping.Key - i) * grouping.Count());
+
+    private int SolveFor2(IGrouping<int, int>[] crabs, int i) =>
+        crabs.Sum(grouping => Gauss(Math.Abs(grouping.Key - i)) * grouping.Count());
+
+    private int Gauss(int i) => i * (i + 1) / 2;
+}
---
+++ tests/AdventOfCode.Tests/Year2021/Day07Tests.cs
@@ -0,0 +1,29 @@
+﻿using AdventOfCode.Year2021;
+
+using FluentAssertions;
+
+namespace AdventOfCode.Tests.Year2021;
+
+public class Day07Tests : IClassFixture<Day07>
+{
+    private readonly Day07 _sut;
+
+    private const string TestData = "16,1,2,0,4,2,7,1,2,14";
+
+    public Day07Tests(Day07 sut)
+    {
+        _sut = sut;
+    }
+
+    [Fact(DisplayName = "Part1 should return expected results from example data")]
+    public void Part1Example()
+    {
+        _sut.Part1(TestData).As<int>().Should().Be(37);
+    }
+
+    [Fact(DisplayName = "Part2 should return expected results from example data")]
+    public void Part2Example()
+    {
+        _sut.Part2(TestData).As<int>().Should().Be(168);
+    }
+}
