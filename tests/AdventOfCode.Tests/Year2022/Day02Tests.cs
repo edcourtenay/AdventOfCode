16fa012eccb2c92675533ec88158ce5dc6bf56fc
Ed Courtenay <ed@edcourtenay.co.uk>
2022-12-03 17:52:22 +0000

    Update tests

--- tests/AdventOfCode.Tests/Year2022/Day02Tests.cs
+++ tests/AdventOfCode.Tests/Year2022/Day02Tests.cs
@@ -6,25 +6,28 @@ namespace AdventOfCode.Tests.Year2022;
 
 public class Day02Tests : IClassFixture<Day02>
 {
+    private readonly Day02 _sut;
+
     private const string TestData = """
     A Y
     B X
     C Z
     """;
 
+    public Day02Tests(Day02 sut)
+    {
+        _sut = sut;
+    }
+
     [Fact(DisplayName = "Part1 should return expected results from example data")]
     public void Part1Example()
     {
-        var sut = new Day02();
-
-        sut.Part1(TestData).As<int>().Should().Be(15);
+        _sut.Part1(TestData).As<int>().Should().Be(15);
     }
 
     [Fact(DisplayName = "Part2 should return expected results from example data")]
     public void Part2Example()
     {
-        var sut = new Day02();
-
-        sut.Part2(TestData).As<int>().Should().Be(12);
+        _sut.Part2(TestData).As<int>().Should().Be(12);
     }
 }
--- tests/AdventOfCode.Tests/Year2022/Day03Tests.cs
+++ tests/AdventOfCode.Tests/Year2022/Day03Tests.cs
@@ -6,6 +6,8 @@ namespace AdventOfCode.Tests.Year2022;
 
 public class Day03Tests : IClassFixture<Day03>
 {
+    private readonly Day03 _sut;
+
     private const string TestData = """
     vJrwpWtwJgWrhcsFMMfFFhFp
     jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
@@ -15,19 +17,20 @@ public class Day03Tests : IClassFixture<Day03>
     CrZsJsPPZsGzwwsLwLmpwMDw
     """;
 
+    public Day03Tests(Day03 sut)
+    {
+        _sut = sut;
+    }
+
     [Fact(DisplayName = "Part1 should return expected results from example data")]
     public void Part1Example()
     {
-        var sut = new Day03();
-
-        sut.Part1(TestData).As<int>().Should().Be(157);
+        _sut.Part1(TestData).As<int>().Should().Be(157);
     }
 
     [Fact(DisplayName = "Part2 should return expected results from example data")]
     public void Part2Example()
     {
-        var sut = new Day03();
-
-        sut.Part2(TestData).As<int>().Should().Be(70);
+        _sut.Part2(TestData).As<int>().Should().Be(70);
     }
 }
