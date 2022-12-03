5defea059a55d57c5d382dc3c420606c11f1e250
Ed Courtenay <ed@edcourtenay.co.uk>
2022-12-03 18:26:56 +0000

    Template for 2022 Day 07

---
+++ src/AdventOfCode/Year2022/Day07.cs
@@ -0,0 +1,9 @@
+﻿namespace AdventOfCode.Year2022;
+
+[Description("Puzzle Title")]
+public class Day04 : IPuzzle
+{
+    public object Part1(string input) => string.Empty;
+
+    public object Part2(string input) => string.Empty;
+}
