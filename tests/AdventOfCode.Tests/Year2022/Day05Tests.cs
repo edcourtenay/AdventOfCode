f51ad5d8e54f8433a8f34d5aa587f1d5a5508729
Ed Courtenay <ed@edcourtenay.co.uk>
2022-12-03 17:57:14 +0000

    Template for 2022 Day 05

---
+++ tests/AdventOfCode.Tests/Year2022/Day05Tests.cs
@@ -0,0 +1,29 @@
+﻿using AdventOfCode.Year2022;
+
+namespace AdventOfCode.Tests.Year2022;
+
+public class Day05Tests : IClassFixture<Day05>
+{
+    private readonly Day05 _sut;
+
+    private const string TestData = """
+    Insert Test Data Here
+    """;
+
+    public Day05Tests(Day05 sut)
+    {
+        _sut = sut;
+    }
+
+    [Fact(DisplayName = "Part1 should return expected results from example data")]
+    public void Part1Example()
+    {
+        _sut.Part1(TestData).As<int>().Should().Be(-1);
+    }
+
+    [Fact(DisplayName = "Part2 should return expected results from example data")]
+    public void Part2Example()
+    {
+        _sut.Part2(TestData).As<int>().Should().Be(-1);
+    }
+}
