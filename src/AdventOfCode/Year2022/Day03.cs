576d3c43b0c8c81bd71289acd8a62fe212b9c7e4
Ed Courtenay <ed@edcourtenay.co.uk>
2022-12-03 14:56:07 +0000

    Completed 2022 Day 03

---
+++ src/AdventOfCode/Year2022/Day03.cs
@@ -0,0 +1,27 @@
+﻿namespace AdventOfCode.Year2022;
+
+[Description("Rucksack Reorganization")]
+public class Day03 : IPuzzle
+{
+    public object Part1(string input) =>
+        Execute(input, s => s.ToLines(SplitIntoCompartments));
+
+    public object Part2(string input) =>
+        Execute(input, s => input.ToLines().Window(3));
+
+    private static int Execute(string input, Func<string, IEnumerable<IEnumerable<IEnumerable<char>>>> func) =>
+        func(input)
+            .Select(strings => strings.Aggregate((prev, current) => current.Intersect(prev)).Single())
+            .Sum(c => c switch
+            {
+                >= 'a' and <= 'z' => c - 'a' + 1,
+                >= 'A' and <= 'Z' => c - 'A' + 27,
+                _ => throw new ArgumentOutOfRangeException(nameof(c), c, null)
+            });
+
+    private static string[] SplitIntoCompartments(string arg)
+    {
+        int compartmentLength = arg.Length / 2;
+        return new [] { arg[..compartmentLength], arg[^compartmentLength..] };
+    }
+}
---
+++ tests/AdventOfCode.Tests/Year2022/Day03Tests.cs
@@ -0,0 +1,33 @@
+﻿using AdventOfCode.Year2022;
+
+using FluentAssertions;
+
+namespace AdventOfCode.Tests.Year2022;
+
+public class Day03Tests : IClassFixture<Day03>
+{
+    private const string TestData = """
+    vJrwpWtwJgWrhcsFMMfFFhFp
+    jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+    PmmdzqPrVvPwwTWBwg
+    wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+    ttgJtRGJQctTZtZT
+    CrZsJsPPZsGzwwsLwLmpwMDw
+    """;
+
+    [Fact(DisplayName = "Part1 should return expected results from example data")]
+    public void Part1Example()
+    {
+        var sut = new Day03();
+
+        sut.Part1(TestData).As<int>().Should().Be(157);
+    }
+
+    [Fact(DisplayName = "Part2 should return expected results from example data")]
+    public void Part2Example()
+    {
+        var sut = new Day03();
+
+        sut.Part2(TestData).As<int>().Should().Be(70);
+    }
+}
