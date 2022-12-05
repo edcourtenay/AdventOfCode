9f236822b0d17e449ff22c9403622d202b0184a0
Ed Courtenay <ed@edcourtenay.co.uk>
2022-12-05 10:18:10 +0000

    Completed 2022 Day 05 (#4)

---
+++ src/AdventOfCode/Year2022/Day05.cs
@@ -0,0 +1,77 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Year2022;
+
+[Description("Supply Stacks")]
+public partial class Day05 : IPuzzle
+{
+    public object Part1(string input) => ParseInput(input, Part1Strategy);
+
+    public object Part2(string input) => ParseInput(input, Part2Strategy);
+
+    public static string ParseInput(string input, Action<Stack<char>[], int, int, int> movementStrategy)
+    {
+        var dataSection = input.ToLines()
+            .ToSequences(string.IsNullOrEmpty).GetEnumerator();
+
+        dataSection.MoveNext();
+        var stacks = ParseStacks(dataSection.Current);
+
+        dataSection.MoveNext();
+        ExecuteInstructions(dataSection, stacks, movementStrategy);
+
+        return new string(stacks.Select(stack => stack.Pop()).ToArray());
+    }
+
+    private static Stack<char>[] ParseStacks(IEnumerable<string> strings)
+    {
+        var arr = strings.ToArray();
+        var maxLength = arr.Max(s => s.Length);
+
+        return arr.Select(s => s.PadRight(maxLength, ' '))
+            .Pivot().Select(chars => new string(chars.Reverse().ToArray())).Where(s1 => s1.Length != 0 && char.IsDigit(s1[0]))
+            .Select(s2 => new Stack<char>(s2.Trim()[1..])).ToArray();
+    }
+
+    private static void ExecuteInstructions(IEnumerator<IEnumerable<string>> enumerable, Stack<char>[] stacks,
+        Action<Stack<char>[], int, int, int> movementStrategy)
+    {
+        foreach (string instruction in enumerable.Current)
+        {
+            if (InstructionRegex().Match(instruction) is not { Success: true } match)
+            {
+                continue;
+            }
+
+            int quantity = int.Parse(match.Groups["quantity"].Value);
+            int source = int.Parse(match.Groups["source"].Value) - 1;
+            var destination = int.Parse(match.Groups["destination"].Value) - 1;
+            movementStrategy(stacks, quantity, destination, source);
+        }
+    }
+
+    private static void Part1Strategy(Stack<char>[] stacks, int quantity, int destination, int source)
+    {
+        for (int i = 0; i < quantity; i++)
+        {
+            stacks[destination].Push(stacks[source].Pop());
+        }
+    }
+
+    private static void Part2Strategy(Stack<char>[] stacks, int quantity, int destination, int source)
+    {
+        var tempStack = new Stack<char>();
+        for (int i = 0; i < quantity; i++)
+        {
+            tempStack.Push(stacks[source].Pop());
+        }
+
+        foreach (char c in tempStack)
+        {
+            stacks[destination].Push(c);
+        }
+    }
+
+    [GeneratedRegex("move (?<quantity>\\d+) from (?<source>\\d+) to (?<destination>\\d+)", RegexOptions.Compiled)]
+    private static partial Regex InstructionRegex();
+}
---
+++ tests/AdventOfCode.Tests/Year2022/Day05Tests.cs
@@ -0,0 +1,37 @@
+﻿using AdventOfCode.Year2022;
+
+namespace AdventOfCode.Tests.Year2022;
+
+public class Day05Tests : IClassFixture<Day05>
+{
+    private readonly Day05 _sut;
+
+    private const string TestData = """
+        [D]
+    [N] [C]
+    [Z] [M] [P]
+     1   2   3
+
+    move 1 from 2 to 1
+    move 3 from 1 to 3
+    move 2 from 2 to 1
+    move 1 from 1 to 2
+    """;
+
+    public Day05Tests(Day05 sut)
+    {
+        _sut = sut;
+    }
+
+    [Fact(DisplayName = "Part1 should return expected results from example data")]
+    public void Part1Example()
+    {
+        _sut.Part1(TestData).As<string>().Should().Be("CMZ");
+    }
+
+    [Fact(DisplayName = "Part2 should return expected results from example data")]
+    public void Part2Example()
+    {
+        _sut.Part2(TestData).As<string>().Should().Be("MCD");
+    }
+}
