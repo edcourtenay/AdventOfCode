e0adbd9bb00766af68c270fabcaa7be313dd7816
Ed Courtenay <ed@edcourtenay.co.uk>
2022-12-05 21:27:55 +0000

    Completed 2019 Day 03

---
+++ tests/AdventOfCode.Tests/Year2019/Day03Tests.cs
@@ -0,0 +1,30 @@
+﻿using AdventOfCode.Year2019;
+
+namespace AdventOfCode.Tests.Year2019;
+
+public class Day03Tests : IClassFixture<Day03>
+{
+    private readonly Day03 _sut;
+
+    private const string TestData = """
+    R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
+    U98,R91,D20,R16,D67,R40,U7,R15,U6,R7
+    """;
+
+    public Day03Tests(Day03 sut)
+    {
+        _sut = sut;
+    }
+
+    [Fact(DisplayName = "Part1 should return expected results from example data")]
+    public void Part1Example()
+    {
+        _sut.Part1(TestData).As<int>().Should().Be(135);
+    }
+
+    [Fact(DisplayName = "Part2 should return expected results from example data")]
+    public void Part2Example()
+    {
+        _sut.Part2(TestData).As<int>().Should().Be(410);
+    }
+}
