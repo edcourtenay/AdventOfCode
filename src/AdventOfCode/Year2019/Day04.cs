d32ebb3b48673557d008d9693789ec9e6aa6a047
Ed Courtenay <ed@edcourtenay.co.uk>
2022-12-05 23:19:04 +0000

    Completed 2019 Day 04

---
+++ src/AdventOfCode/Year2019/Day04.cs
@@ -0,0 +1,48 @@
+﻿namespace AdventOfCode.Year2019;
+
+[Description("Secure Container")]
+public class Day04 : IPuzzle
+{
+    public object Part1(string input)
+    {
+        return Solve(input, i => IsValid(i.ToString()));
+    }
+
+    public object Part2(string input)
+    {
+        return Solve(input, i => IsValid2(i.ToString()));
+    }
+
+    private static object Solve(string input, Func<int, bool> predicate)
+    {
+        var range = input.Split('-').Select(int.Parse).ToArray();
+        return Enumerable.Range(range[0], range[1] - range[0] + 1)
+            .Count(predicate);
+    }
+
+    private static bool IsValid(string input)
+    {
+        bool allAscending = input.Pairwise().All(tuple => tuple.First <= tuple.Second);
+        return allAscending && input.Pairwise().Any(tuple => tuple.First == tuple.Second);
+    }
+
+    public static bool IsValid2(string input)
+    {
+        var pairsSeen = new int[10];
+
+        foreach ((char first, char second) in input.Pairwise())
+        {
+            if (first > second)
+            {
+                return false;
+            }
+
+            if (first == second)
+            {
+                pairsSeen[first - '0'] += 1;
+            }
+        }
+
+        return pairsSeen.Any(i => i == 1);
+    }
+}
---
+++ tests/AdventOfCode.Tests/Year2019/Day04Tests.cs
@@ -0,0 +1,15 @@
+﻿using AdventOfCode.Year2019;
+
+namespace AdventOfCode.Tests.Year2019;
+
+public class Day04Tests
+{
+    [Theory(DisplayName = "IsValid2 should produce expected results")]
+    [InlineData("112233", true)]
+    [InlineData("123444", false)]
+    [InlineData("111122", true)]
+    public void IsValid2Tests(string input, bool expected)
+    {
+        Day04.IsValid2(input).Should().Be(expected);
+    }
+}
