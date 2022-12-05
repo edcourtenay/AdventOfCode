9cd0903d0fde3cdfdc4ba9d550e9d33f6304b6ac
Ed Courtenay <ed@edcourtenay.co.uk>
2022-12-05 17:27:56 +0000

    Completed 2019 Day 02

---
+++ src/AdventOfCode/Year2019/Day02.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+
+namespace AdventOfCode.Year2019
+{
+    [Description("1202 Program Alarm")]
+    public class Day02 : IPuzzle
+    {
+        public object Part1(string input)
+        {
+            var program = input.Split(',').Select(int.Parse).ToArray();
+            program[1] = 12;
+            program[2] = 2;
+
+            ExecuteProgram(0, program);
+
+            return program[0];
+        }
+
+        public object Part2(string input)
+        {
+            var baseProgram = input.Split(',').Select(int.Parse).ToArray();
+            var program = new int[baseProgram.Length];
+
+            for (int i = 0; i < 100; i++)
+            {
+                for (int j = 0; j < 100; j++)
+                {
+                    baseProgram.CopyTo(program, 0);
+                    program[1] = i;
+                    program[2] = j;
+
+                    ExecuteProgram(0, program);
+
+                    if (program[0] == 19690720)
+                    {
+                        return (100 * i) + j;
+                    }
+                }
+            }
+
+            return 0;
+        }
+
+        public static void ExecuteProgram(int programCounter, int[] program)
+        {
+            while (program[programCounter] != 99)
+            {
+                program[program[programCounter + 3]] = program[programCounter] switch
+                {
+                    1 => program[program[programCounter + 1]] + program[program[programCounter + 2]],
+                    2 => program[program[programCounter + 1]] * program[program[programCounter + 2]],
+                    _ => program[program[programCounter + 3]]
+                };
+
+                programCounter += 4;
+            }
+        }
+    }
+}
---
+++ tests/AdventOfCode.Tests/Year2019/Day02Tests.cs
@@ -0,0 +1,15 @@
+﻿using AdventOfCode.Year2019;
+
+namespace AdventOfCode.Tests.Year2019;
+
+public class Day02Tests
+{
+    [Fact(DisplayName = "Part1 should return expected results from example data")]
+    public void Part1Example()
+    {
+        var program = new[] { 1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50 };
+        Day02.ExecuteProgram(0, program);
+
+        program[0].Should().Be(3500);
+    }
+}
