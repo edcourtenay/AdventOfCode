77abd68be946e2d490af3c81b5da0b7bb8347b63
Ed Courtenay <ed@edcourtenay.co.uk>
2022-12-05 08:08:47 +0000

    Initial data & test for part 1

--- tests/AdventOfCode.Tests/Year2022/Day05Tests.cs
+++ tests/AdventOfCode.Tests/Year2022/Day05Tests.cs
@@ -7,7 +7,15 @@ public class Day05Tests : IClassFixture<Day05>
     private readonly Day05 _sut;
 
     private const string TestData = """
-    Insert Test Data Here
+    [D]
+    [N] [C]
+    [Z] [M] [P]
+     1   2   3
+
+    move 1 from 2 to 1
+    move 3 from 1 to 3
+    move 2 from 2 to 1
+    move 1 from 1 to 2
     """;
 
     public Day05Tests(Day05 sut)
@@ -18,12 +26,12 @@ public class Day05Tests : IClassFixture<Day05>
     [Fact(DisplayName = "Part1 should return expected results from example data")]
     public void Part1Example()
     {
-        _sut.Part1(TestData).As<int>().Should().Be(-1);
+        _sut.Part1(TestData).As<string>().Should().Be("CMZ");
     }
 
     [Fact(DisplayName = "Part2 should return expected results from example data")]
     public void Part2Example()
     {
-        _sut.Part2(TestData).As<int>().Should().Be(-1);
+        _sut.Part2(TestData).As<string>().Should().Be("CMZ");
     }
 }
