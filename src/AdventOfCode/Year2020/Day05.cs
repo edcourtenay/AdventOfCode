9733de28d636721f1a8dac5127953feb7b0fc054
Ed Courtenay <ed@edcourtenay.co.uk>
2022-12-06 21:55:38 +0000

    Completed 2020 Day 05

---
+++ src/AdventOfCode/Year2020/Day05.cs
@@ -0,0 +1,38 @@
+﻿namespace AdventOfCode.Year2020;
+
+[Description("Binary Boarding")]
+public class Day05 : IPuzzle
+{
+    public object Part1(string input)
+    {
+        return input.ToLines(Parse)
+            .Max();
+    }
+
+    public object Part2(string input)
+    {
+        return input.ToLines(Parse)
+            .Order()
+            .Pairwise()
+            .First(tuple => tuple.First + 1 == tuple.Second - 1)
+            .First + 1;
+    }
+
+    public static int Parse(string line)
+    {
+        var row = Process(line[..7], 127);
+        var col = Process(line[^3..], 7);
+
+        return row * 8 + col;
+    }
+
+    private static int Process(string data, int high)
+    {
+        return data.Aggregate((low: 0, high), (current, c) => c switch
+        {
+            'F' or 'L' => (current.low, current.high - (current.high - current.low) / 2 - 1),
+            'B' or 'R' => (current.low + (current.high - current.low) / 2 + 1, current.high),
+            _ => throw new ArgumentOutOfRangeException()
+        }).low;
+    }
+}
---
+++ tests/AdventOfCode.Tests/Year2020/Day05Tests.cs
@@ -0,0 +1,16 @@
+﻿using AdventOfCode.Year2020;
+
+namespace AdventOfCode.Tests.Year2020;
+
+public class Day05Tests
+{
+    [Theory(DisplayName = "Parse should produce expected results")]
+    [InlineData("FBFBBFFRLR", 357)]
+    [InlineData("BFFFBBFRRR", 567)]
+    [InlineData("FFFBBBFRRR", 119)]
+    [InlineData("BBFFBBFRLL", 820)]
+    public void ParseTests(string line, int seatId)
+    {
+        Day05.Parse(line).Should().Be(seatId);
+    }
+}
