81cddda950068466e185f5da7d9749131fe53fb5
Ed Courtenay <ed@edcourtenay.co.uk>
2022-12-06 22:57:45 +0000

    Completed 2020 Day 06

---
+++ tests/AdventOfCode.Tests/Year2020/Day06Tests.cs
@@ -0,0 +1,43 @@
+﻿using AdventOfCode.Year2020;
+
+namespace AdventOfCode.Tests.Year2020;
+
+public class Day06Tests : IClassFixture<Day06>
+{
+    private readonly Day06 _sut;
+
+    private const string TestData = """
+    abc
+
+    a
+    b
+    c
+
+    ab
+    ac
+
+    a
+    a
+    a
+    a
+
+    b
+    """;
+
+    public Day06Tests(Day06 sut)
+    {
+        _sut = sut;
+    }
+
+    [Fact(DisplayName = "Part1 should return expected results from example data")]
+    public void Part1Example()
+    {
+        _sut.Part1(TestData).As<int>().Should().Be(11);
+    }
+
+    [Fact(DisplayName = "Part2 should return expected results from example data")]
+    public void Part2Example()
+    {
+        _sut.Part2(TestData).As<int>().Should().Be(6);
+    }
+}
