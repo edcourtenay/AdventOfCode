48e51383d77d97e6b8f535194878f5dba8053d0f
Ed Courtenay <ed@edcourtenay.co.uk>
2022-12-06 08:43:39 +0000

    Completed 2022 Day 06 (#5)

---
+++ tests/AdventOfCode.Tests/Year2022/Day06Tests.cs
@@ -0,0 +1,35 @@
+﻿using AdventOfCode.Year2022;
+
+namespace AdventOfCode.Tests.Year2022;
+
+public class Day06Tests : IClassFixture<Day06>
+{
+    private readonly Day06 _sut;
+
+    public Day06Tests(Day06 sut)
+    {
+        _sut = sut;
+    }
+
+    [Theory(DisplayName = "Part1 should return expected results from example data")]
+    [InlineData("mjqjpqmgbljsphdzintnvjfqwrcgsmlb", 7)]
+    [InlineData("bvwbjplbgvbhsrlpgdmjqwftvncz", 5)]
+    [InlineData("nppdvjthqldpwncqszvftbrmjlhg", 6)]
+    [InlineData("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10)]
+    [InlineData("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11)]
+    public void Part1Example(string input, int expected)
+    {
+        _sut.Part1(input).As<int>().Should().Be(expected);
+    }
+
+    [Theory(DisplayName = "Part2 should return expected results from example data")]
+    [InlineData("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19)]
+    [InlineData("bvwbjplbgvbhsrlpgdmjqwftvncz", 23)]
+    [InlineData("nppdvjthqldpwncqszvftbrmjlhg", 23)]
+    [InlineData("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29)]
+    [InlineData("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26)]
+    public void Part2Example(string input, int expected)
+    {
+        _sut.Part2(input).As<int>().Should().Be(expected);
+    }
+}
