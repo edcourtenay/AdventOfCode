3ff29e69e03c18a652eb0017173d2bc8313c1459
Ed Courtenay <ed@edcourtenay.co.uk>
2022-12-07 22:26:00 +0000

    Completed 2016 Day 07

---
+++ src/AdventOfCode/Year2016/Day07.cs
@@ -0,0 +1,61 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Year2016;
+
+[Description("Internet Protocol Version 7")]
+public partial class Day07 : IPuzzle
+{
+    public object Part1(string input)
+    {
+        return input.ToLines()
+            .Count(SupportsTls);
+    }
+
+    public object Part2(string input)
+    {
+        return input.ToLines()
+            .Count(SupportsSsl);
+    }
+
+    public static bool SupportsTls(string line)
+    {
+        var arr = line.Split('[', ']');
+
+        return arr.Where((s, i) => i % 2 == 0).Any(HasAbba)
+               && !arr.Where((s, i) => i % 2 != 0).Any(HasAbba);
+    }
+
+    public static bool HasAbba(string text)
+    {
+        return text.SlidingWindow(4)
+            .Select(chars => chars.ToArray())
+            .Any(chars => chars[0] == chars[3] && chars[1] == chars[2]
+                && chars[0] != chars[1]);
+    }
+
+    public static bool SupportsSsl(string line)
+    {
+        var arr = line.Split('[', ']');
+
+        var x = arr.Where((s, i) => i % 2 == 0)
+            .SelectMany(FindAba)
+            .Select(chars =>
+            {
+                chars[0] = chars[1];
+                chars[1] = chars[2];
+                chars[2] = chars[0];
+
+                return new string(chars);
+            });
+
+        return x.Any(z => arr.Where((s, i) => i % 2 != 0).Any(v => v.Contains(z)));
+    }
+
+    public static IEnumerable<char[]> FindAba(string input)
+    {
+        return input.SlidingWindow(3)
+            .Select(chars => chars.ToArray())
+            .Where(chars => chars[0] == chars[2] && chars[0] != chars[1]);
+    }
+
+}
---
+++ tests/AdventOfCode.Tests/Year2016/Day07Tests.cs
@@ -0,0 +1,27 @@
+﻿using AdventOfCode.Year2016;
+
+namespace AdventOfCode.Tests.Year2016;
+
+public class Day07Tests
+{
+    [Theory(DisplayName = "SupportsTls should return expected values")]
+    [InlineData("abba[mnop]qrst", true)]
+    [InlineData("abcd[bddb]xyyx", false)]
+    [InlineData("aaaa[qwer]tyui", false)]
+    [InlineData("ioxxoj[asdfgh]zxcvbn", true)]
+    [InlineData("ioxxoj[asdfghbccb]zxcvbn", false)]
+    public void SupportsTlsTests(string input, bool expected)
+    {
+        Day07.SupportsTls(input).Should().Be(expected);
+    }
+
+    [Theory(DisplayName = "SupportsSsl should return expected values")]
+    [InlineData("aba[bab]xyz", true)]
+    [InlineData("xyx[xyx]xyx", false)]
+    [InlineData("aaa[kek]eke", true)]
+    [InlineData("zazbz[bzb]cdb", true)]
+    public void SupportsSslTests(string input, bool expected)
+    {
+        Day07.SupportsSsl(input).Should().Be(expected);
+    }
+}
