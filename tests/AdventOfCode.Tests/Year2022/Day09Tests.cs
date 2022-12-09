21d048de94fa291280382f70465df2298d127f1f
Ed Courtenay <ed@edcourtenay.co.uk>
2022-12-09 22:06:39 +0000

    Completed 2022 Day 09

---
+++ tests/AdventOfCode.Tests/Year2022/Day09Tests.cs
@@ -0,0 +1,38 @@
+﻿using AdventOfCode.Year2022;
+
+using FluentAssertions;
+
+namespace AdventOfCode.Tests.Year2022;
+
+public class Day09Tests : IClassFixture<Day09>
+{
+    private readonly Day09 _sut;
+
+    private const string TestData = """
+    R 4
+    U 4
+    L 3
+    D 1
+    R 4
+    D 1
+    L 5
+    R 2
+    """;
+
+    public Day09Tests(Day09 sut)
+    {
+        _sut = sut;
+    }
+
+    [Fact(DisplayName = "Part1 should return expected results from example data")]
+    public void Part1Example()
+    {
+        _sut.Part1(TestData).As<int>().Should().Be(13);
+    }
+
+    [Fact(DisplayName = "Part2 should return expected results from example data")]
+    public void Part2Example()
+    {
+        _sut.Part2(TestData).As<int>().Should().Be(1);
+    }
+}
