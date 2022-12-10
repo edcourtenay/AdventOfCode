c00b691c2393dc1ab31a6dba0fec603a3e286c0b
Ed Courtenay <ed@edcourtenay.co.uk>
2022-12-10 18:10:38 +0000

    Completed 2022 Day 10

---
+++ src/AdventOfCode/Year2022/Day10.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+
+namespace AdventOfCode.Year2022;
+
+[Description("Cathode-Ray Tube")]
+public class Day10 : IPuzzle
+{
+    public object Part1(string input)
+    {
+        return ProcessInstructions(input.ToLines())
+            .Select((x, i) => (x, i: i + 1))
+            .Where(t => (t.i - 20) % 40 == 0)
+            .Sum(tuple => tuple.x * tuple.i);
+    }
+
+    public object Part2(string input)
+    {
+        var registers = ProcessInstructions(input.ToLines());
+
+        var sb = new StringBuilder();
+        sb.AppendLine();
+        var x = 0;
+        using IEnumerator<int> enumerator = registers.GetEnumerator();
+
+        while (enumerator.MoveNext())
+        {
+            sb.Append(x >= enumerator.Current - 1
+                      && x <= enumerator.Current + 1 ? '#' : '.');
+
+            if (++x == 40)
+            {
+                x = 0;
+                sb.AppendLine();
+            }
+        }
+
+        return sb.ToString();
+    }
+
+    private static IEnumerable<int> ProcessInstructions(IEnumerable<string> instructions)
+    {
+        var x = 1;
+
+        foreach (string instruction in instructions)
+        {
+            var segments = instruction.Split(' ');
+
+            (int cycles, int op1, Func<int, int, int> func) t = segments switch
+            {
+                ["noop"] => (1, 0, (r, _) => r),
+                ["addx", { } p1] => (2, int.Parse(p1), (r, op1) => r + op1),
+                _ => throw new ArgumentException()
+            };
+
+            for (int i = 0; i < t.cycles; i++)
+            {
+                yield return x;
+            }
+
+            x = t.func(x, t.op1);
+        }
+    }
+}
---
+++ tests/AdventOfCode.Tests/Year2022/Day10Tests.cs
@@ -0,0 +1,184 @@
+﻿using AdventOfCode.Year2022;
+
+namespace AdventOfCode.Tests.Year2022;
+
+public class Day10Tests : IClassFixture<Day10>
+{
+    private readonly Day10 _sut;
+
+    private const string TestData = """
+    addx 15
+    addx -11
+    addx 6
+    addx -3
+    addx 5
+    addx -1
+    addx -8
+    addx 13
+    addx 4
+    noop
+    addx -1
+    addx 5
+    addx -1
+    addx 5
+    addx -1
+    addx 5
+    addx -1
+    addx 5
+    addx -1
+    addx -35
+    addx 1
+    addx 24
+    addx -19
+    addx 1
+    addx 16
+    addx -11
+    noop
+    noop
+    addx 21
+    addx -15
+    noop
+    noop
+    addx -3
+    addx 9
+    addx 1
+    addx -3
+    addx 8
+    addx 1
+    addx 5
+    noop
+    noop
+    noop
+    noop
+    noop
+    addx -36
+    noop
+    addx 1
+    addx 7
+    noop
+    noop
+    noop
+    addx 2
+    addx 6
+    noop
+    noop
+    noop
+    noop
+    noop
+    addx 1
+    noop
+    noop
+    addx 7
+    addx 1
+    noop
+    addx -13
+    addx 13
+    addx 7
+    noop
+    addx 1
+    addx -33
+    noop
+    noop
+    noop
+    addx 2
+    noop
+    noop
+    noop
+    addx 8
+    noop
+    addx -1
+    addx 2
+    addx 1
+    noop
+    addx 17
+    addx -9
+    addx 1
+    addx 1
+    addx -3
+    addx 11
+    noop
+    noop
+    addx 1
+    noop
+    addx 1
+    noop
+    noop
+    addx -13
+    addx -19
+    addx 1
+    addx 3
+    addx 26
+    addx -30
+    addx 12
+    addx -1
+    addx 3
+    addx 1
+    noop
+    noop
+    noop
+    addx -9
+    addx 18
+    addx 1
+    addx 2
+    noop
+    noop
+    addx 9
+    noop
+    noop
+    noop
+    addx -1
+    addx 2
+    addx -37
+    addx 1
+    addx 3
+    noop
+    addx 15
+    addx -21
+    addx 22
+    addx -6
+    addx 1
+    noop
+    addx 2
+    addx 1
+    noop
+    addx -10
+    noop
+    noop
+    addx 20
+    addx 1
+    addx 2
+    addx 2
+    addx -6
+    addx -11
+    noop
+    noop
+    noop
+    """;
+
+    public Day10Tests(Day10 sut)
+    {
+        _sut = sut;
+    }
+
+    [Fact(DisplayName = "Part1 should return expected results from example data")]
+    public void Part1Example()
+    {
+        _sut.Part1(TestData).As<int>().Should().Be(13140);
+    }
+
+    [Fact(DisplayName = "Part2 should return expected results from example data")]
+    public void Part2Example()
+    {
+        var expectedResult = """
+
+        ##..##..##..##..##..##..##..##..##..##..
+        ###...###...###...###...###...###...###.
+        ####....####....####....####....####....
+        #####.....#####.....#####.....#####.....
+        ######......######......######......####
+        #######.......#######.......#######.....
+
+        """;
+        _sut.Part2(TestData).As<string>().Should().Be(expectedResult);
+    }
+}
