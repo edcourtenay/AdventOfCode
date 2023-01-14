55fa1b8f7a05a6ec01002ebfab719e37c9a5395b
Ed Courtenay <ed@edcourtenay.co.uk>
2023-01-14 10:35:33 +0000

    Completed 2018 Day 02

---
+++ src/AdventOfCode/Year2018/Day02.cs
@@ -0,0 +1,43 @@
+﻿namespace AdventOfCode.Year2018;
+
+[Description("Inventory Management System")]
+public class Day02 : IPuzzle
+{
+    public object Part1(string input)
+    {
+        var results = input.ToLines(s => new { Two = ContainsRepeatedChars(s, 2), Three = ContainsRepeatedChars(s, 3) }).ToArray();
+
+        var count2 = results.Count(arg => arg.Two);
+        var count3 = results.Count(arg => arg.Three);
+
+        return count2 * count3;
+    }
+
+    public object Part2(string input)
+    {
+        List<string> boxIds = input.ToLines().ToList();
+        for (int i = 0; i < boxIds.Count; i++) {
+            for (int j = i + 1; j < boxIds.Count; j++) {
+                int diffCount = 0;
+                int diffIndex = 0;
+                for (int k = 0; k < boxIds[i].Length; k++)
+                {
+                    if (boxIds[i][k] == boxIds[j][k])
+                    {
+                        continue;
+                    }
+
+                    diffCount++;
+                    diffIndex = k;
+                    if (diffCount > 1) break;
+                }
+                if (diffCount == 1) {
+                    return boxIds[i].Remove(diffIndex, 1);
+                }
+            }
+        }
+        return "";
+    }
+
+    public bool ContainsRepeatedChars(string input, int repeat) => input.GroupBy(c => c).Any(g => g.Count() == repeat);
+}
---
+++ tests/AdventOfCode.Tests/Year2018/Day02Tests.cs
@@ -0,0 +1,45 @@
+﻿using AdventOfCode.Year2018;
+
+namespace AdventOfCode.Tests.Year2018;
+
+public class Day02Tests : IClassFixture<Day02>
+{
+    private readonly Day02 _sut;
+
+    private const string Part1TestData = """
+    abcdef
+    bababc
+    abbcde
+    abcccd
+    aabcdd
+    abcdee
+    ababab
+    """;
+
+    private const string Part2TestData = """
+    abcde
+    fghij
+    klmno
+    pqrst
+    fguij
+    axcye
+    wvxyz
+    """;
+
+    public Day02Tests(Day02 sut)
+    {
+        _sut = sut;
+    }
+
+    [Fact(DisplayName = "Part1 should return expected results from example data")]
+    public void Part1Example()
+    {
+        _sut.Part1(Part1TestData).As<int>().Should().Be(12);
+    }
+
+    [Fact(DisplayName = "Part2 should return expected results from example data")]
+    public void Part2Example()
+    {
+        _sut.Part2(Part2TestData).As<string>().Should().Be("fgij");
+    }
+}
