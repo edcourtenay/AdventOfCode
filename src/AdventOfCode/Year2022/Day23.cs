ba88053b4380974dd459751c6846e05654d27799
Ed Courtenay <ed@edcourtenay.co.uk>
2023-01-19 17:16:10 +0000

    Initial stuff

---
+++ src/AdventOfCode/Year2022/Day23.cs
@@ -0,0 +1,37 @@
+﻿namespace AdventOfCode.Year2022;
+
+[Description("Unstable Diffusion")]
+public class Day23 : IPuzzle
+{
+    public object Part1(string input)
+    {
+        var set = new HashSet<(int x, int y)>(ParseData(input));
+
+        return string.Empty;
+    }
+
+    public object Part2(string input)
+    {
+        return string.Empty;
+    }
+
+    private static IEnumerable<(int x, int y)> ParseData(string input)
+    {
+        int y = 0;
+        foreach (string line in input.ToLines())
+        {
+            int x = 0;
+            foreach (char ch in line)
+            {
+                if (ch == '#')
+                {
+                    yield return (x, y);
+                }
+
+                x++;
+            }
+
+            y++;
+        }
+    }
+}
---
+++ tests/AdventOfCode.Tests/Year2022/Day23Tests.cs
@@ -0,0 +1,35 @@
+﻿using AdventOfCode.Year2022;
+
+namespace AdventOfCode.Tests.Year2022;
+
+public class Day23Tests : IClassFixture<Day23>
+{
+    private readonly Day23 _sut;
+
+    private const string TestData = """
+    ....#..
+    ..###.#
+    #...#.#
+    .#...##
+    #.###..
+    ##.#.##
+    .#..#..
+    """;
+
+    public Day23Tests(Day23 sut)
+    {
+        _sut = sut;
+    }
+
+    [Fact(DisplayName = "Part1 should return expected results from example data")]
+    public void Part1Example()
+    {
+        _sut.Part1(TestData).As<int>().Should().Be(110);
+    }
+
+    [Fact(DisplayName = "Part2 should return expected results from example data")]
+    public void Part2Example()
+    {
+        _sut.Part2(TestData).As<int>().Should().Be(301);
+    }
+}
