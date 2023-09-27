31766bf35c95124f947cc28b29c215617c11134d
Ed Courtenay <ed@edcourtenay.co.uk>
2023-09-27 21:46:04 +0100

    Completed 2017 Day 02
    (cherry picked from commit 2c37a8677506b888ca46f37444804d1a9f9736fd)

---
+++ tests/AdventOfCode.Tests/Year2017/Day02Tests.cs
@@ -0,0 +1,35 @@
+﻿using AdventOfCode.Year2017;
+
+namespace AdventOfCode.Tests.Year2017;
+
+public class Day02Tests : IClassFixture<Day02>
+{
+    private readonly Day02 _sut;
+
+    public Day02Tests(Day02 sut)
+    {
+        _sut = sut;
+    }
+
+    [Fact(DisplayName = "Part1 should return expected results from example data")]
+    public void Part1Example()
+    {
+        string input = """
+                       5	1	9	5
+                       7	5	3
+                       2	4	6	8
+                       """;
+        _sut.Part1(input).As<int>().Should().Be(18);
+    }
+
+    [Fact(DisplayName = "Part2 should return expected results from example data")]
+    public void Part2Example()
+    {
+        string input = """
+                       5	9	2	8
+                       9	4	7	3
+                       3	8	6	5
+                       """;
+        _sut.Part2(input).As<int>().Should().Be(9);
+    }
+}
