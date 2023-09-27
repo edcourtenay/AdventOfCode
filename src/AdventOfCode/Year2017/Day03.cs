ead6f25a0c5fead6c2ab15b0695a82166a48261a
Ed Courtenay <ed@edcourtenay.co.uk>
2023-09-27 23:26:41 +0100

    Part 1 Complete

---
+++ src/AdventOfCode/Year2017/Day03.cs
@@ -0,0 +1,37 @@
+﻿namespace AdventOfCode.Year2017;
+
+[Description("Spiral Memory")]
+public class Day03 : IPuzzle
+{
+    public object Part1(string input)
+    {
+        var sideLength = LengthOfSideWith(int.Parse(input));
+        var midpoints = MidpointsForSideLength(sideLength);
+        var stepsToRingFromCenter = (sideLength - 1) / 2;
+        return stepsToRingFromCenter + midpoints.Select(midpoint => Math.Abs(int.Parse(input) - midpoint)).Min();
+    }
+
+    public object Part2(string input)
+    {
+        return string.Empty;
+    }
+
+    private static int LengthOfSideWith(int n)
+    {
+        int i = (int)Math.Round(Math.Sqrt(n));
+        return (i % 2 == 0) switch
+        {
+            true => i + 1,
+            false => i
+        };
+    }
+
+    private int[] MidpointsForSideLength(int sideLength)
+    {
+        var highest = sideLength * sideLength;
+        var offset = (int)((sideLength - 1) / 2.0);
+        return Enumerable.Range(0, 4)
+            .Select(i => highest - (offset + (i * (sideLength - 1))))
+            .ToArray();
+    }
+}
