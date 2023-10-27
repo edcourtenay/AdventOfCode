acfde3d389a7a4a94cdfeaeef6714ccc7cfdf8ba
Ed Courtenay <ed@edcourtenay.co.uk>
2023-10-27 22:04:18 +0100

    Completed 2017 Day 05

---
+++ tests/AdventOfCode.Tests/Year2017/Day05Tests.cs
@@ -0,0 +1,27 @@
+﻿using AdventOfCode.Year2017;
+
+namespace AdventOfCode.Tests.Year2017;
+
+public class Day05Tests : IClassFixture<Day05>
+{
+    private const string StepsInput = "0\n3\n0\n1\n-3";
+    private readonly Day05 _sut;
+
+    public Day05Tests(Day05 sut)
+    {
+        _sut = sut;
+    }
+
+    [Fact(DisplayName = "Part1 should return expected results from example data")]
+    public void Part1Example()
+    {
+        _sut.Part1(StepsInput).As<int>().Should().Be(5);
+    }
+
+    [Fact(DisplayName = "Part2 should return expected results from example data")]
+    public void Part2Example()
+    {
+        _sut.Part2(StepsInput).As<int>().Should().Be(10);
+    }
+
+}
