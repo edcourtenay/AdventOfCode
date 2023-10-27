22a917250f97411fd1b50b33cd8f1abd92fdd45e
Ed Courtenay <ed@edcourtenay.co.uk>
2023-10-27 21:33:39 +0100

    Completed 2017 Day 04 (#31)
    Completed 2017 Day 04

---
+++ tests/AdventOfCode.Tests/Year2017/Day04Tests.cs
@@ -0,0 +1,28 @@
+﻿using AdventOfCode.Year2017;
+
+namespace AdventOfCode.Tests.Year2017;
+
+public class Day04Tests : IClassFixture<Day04>
+{
+    private readonly Day04 _sut;
+
+    public Day04Tests(Day04 sut)
+    {
+        _sut = sut;
+    }
+
+    [Theory(DisplayName = "Validate line should return expected results from example data")]
+    [InlineData("aa bb cc dd ee", true)]
+    [InlineData("aa bb cc dd aa", false)]
+    [InlineData("aa bb cc dd aaa", true)]
+    public void ValidateLineExample(string input, bool expected)
+    {
+        _sut.ValidateLine(input, word => new string(word.ToCharArray().OrderBy(c => c).ToArray())).Should().Be(expected);
+    }
+
+    [Fact]
+    public void Temp()
+    {
+        new string("cba".ToCharArray().OrderBy(c => c).ToArray()).Should().Be("abc");
+    }
+}
