89b05282a9435070e23a5372eec334385053b00e
Ed Courtenay <ed@edcourtenay.co.uk>
2023-10-27 23:35:16 +0100

    Completed 2017 Day 06

---
+++ src/AdventOfCode/Year2017/Day06.cs
@@ -0,0 +1,63 @@
+﻿namespace AdventOfCode.Year2017;
+
+[Description("Memory Reallocation")]
+public class Day06 : IPuzzle
+{
+    public object Part1(string input)
+    {
+        var banks = input.Split(' ', '\t')
+            .Select(int.Parse)
+            .ToArray();
+
+        return Process(banks).Count;
+    }
+
+    public object Part2(string input)
+    {
+        var banks = input.Split(' ', '\t')
+            .Select(int.Parse)
+            .ToArray();
+
+        var found = Process(banks).Banks;
+        return Process(found).Count;
+    }
+
+    private (int Count, int[] Banks) Process(int[] banks)
+    {
+        var seen = new HashSet<string>();
+        do
+        {
+            seen.Add(ToKey(banks));
+
+            int index = FindBank(banks);
+            var units = banks[index];
+            banks[index] = 0;
+
+            while (units > 0)
+            {
+                index = (index + 1) % banks.Length;
+                banks[index]++;
+                units--;
+            }
+        } while (!seen.Contains(ToKey(banks)));
+
+        return (seen.Count, banks);
+    }
+
+    private static string ToKey(int[] ints)
+    {
+        return ints
+            .Select(i => i.ToString())
+            .Aggregate((s0, s1) => $"{s0}-{s1}");
+    }
+
+    private int FindBank(int[] banks)
+    {
+        return banks
+            .Select((a, i) => new { Item = a, Index = i })
+            .OrderByDescending(i => i.Item)
+            .ThenBy(i => i.Index)
+            .First()
+            .Index;
+    }
+}
--- tests/AdventOfCode.Tests/Year2017/Day06Tests.cs
+++ tests/AdventOfCode.Tests/Year2017/Day06Tests.cs
@@ -2,12 +2,11 @@
 
 namespace AdventOfCode.Tests.Year2017;
 
-public class Day05Tests : IClassFixture<Day05>
+public class Day06Tests : IClassFixture<Day06>
 {
-    private const string StepsInput = "0\n3\n0\n1\n-3";
-    private readonly Day05 _sut;
+    private readonly Day06 _sut;
 
-    public Day05Tests(Day05 sut)
+    public Day06Tests(Day06 sut)
     {
         _sut = sut;
     }
@@ -15,12 +14,12 @@ public class Day05Tests : IClassFixture<Day05>
     [Fact(DisplayName = "Part1 should return expected results from example data")]
     public void Part1Example()
     {
-        _sut.Part1(StepsInput).As<int>().Should().Be(5);
+        _sut.Part1("0 2 7 0").As<int>().Should().Be(5);
     }
 
     [Fact(DisplayName = "Part2 should return expected results from example data")]
     public void Part2Example()
     {
-        _sut.Part2(StepsInput).As<int>().Should().Be(10);
+        _sut.Part2("2 4 1 2").As<int>().Should().Be(4);
     }
 }
