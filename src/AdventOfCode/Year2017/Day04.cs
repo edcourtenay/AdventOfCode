28207b26aeae04b07c38274fd618e8dc23a4ed41
Ed Courtenay <ed@edcourtenay.co.uk>
2023-10-27 21:10:26 +0100

    Part 1

---
+++ src/AdventOfCode/Year2017/Day04.cs
@@ -0,0 +1,20 @@
+﻿namespace AdventOfCode.Year2017;
+
+[Description("High-Entropy Passphrases")]
+public class Day04 : IPuzzle
+{
+    public object Part1(string input)
+    {
+        return input.ToLines()
+            .Where(ValidateLine)
+            .Count();
+    }
+
+    public object Part2(string input) => string.Empty;
+
+    public bool ValidateLine(string input)
+    {
+        var words = input.Split(' ');
+        return words.Distinct().Count() == words.Length;
+    }
+}
