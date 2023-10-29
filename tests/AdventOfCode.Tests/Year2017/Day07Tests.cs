a534f7ceb8918ec62b3880c30c17deb649153f43
Ed Courtenay <ed@edcourtenay.co.uk>
2023-10-29 23:26:10 +0000

    Completed 2017 Day 07

---
+++ tests/AdventOfCode.Tests/Year2017/Day07Tests.cs
@@ -0,0 +1,42 @@
+﻿using AdventOfCode.Year2017;
+
+namespace AdventOfCode.Tests.Year2017;
+
+public class Day07Tests : IClassFixture<Day07>
+{
+    private readonly Day07 _sut;
+
+    private static readonly string Part1ExampleData =
+        """
+        pbga (66)
+        xhth (57)
+        ebii (61)
+        havc (66)
+        ktlj (57)
+        fwft (72) -> ktlj, cntj, xhth
+        qoyq (66)
+        padx (45) -> pbga, havc, qoyq
+        tknk (41) -> ugml, padx, fwft
+        jptl (61)
+        ugml (68) -> gyxo, ebii, jptl
+        gyxo (61)
+        cntj (57)
+        """;
+
+    public Day07Tests(Day07 sut)
+    {
+        _sut = sut;
+    }
+
+    [Fact(DisplayName = "Part1 should return expected results from example data")]
+    public void Part1Example()
+    {
+        _sut.Part1(Part1ExampleData).As<string>().Should().Be("tknk");
+    }
+
+    [Fact(DisplayName = "Part2 should return expected results from example data")]
+    public void Part2Example()
+    {
+        _sut.Part2(Part1ExampleData).As<int>().Should().Be(60);
+    }
+}
