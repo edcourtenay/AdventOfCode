4d264303c0170e74f1e71582aa71d98cadf49fbd
Ed Courtenay <ed@edcourtenay.co.uk>
2023-11-30 16:52:09 +0000

    Setup

---
+++ src/AdventOfCode/Year2023/Day01.cs
@@ -0,0 +1,8 @@
+﻿namespace AdventOfCode.Year2023;
+
+public class Day01 : IPuzzle
+{
+    public object Part1(string input) => string.Empty;
+
+    public object Part2(string input) => string.Empty;
+}
---
+++ tests/AdventOfCode.Tests/Year2023/Day01Tests.cs
@@ -0,0 +1,30 @@
+﻿using AdventOfCode.Year2022;
+
+namespace AdventOfCode.Tests.Year2023;
+
+public class Day01Tests : IClassFixture<Day01>
+{
+    private readonly Day01 _sut;
+
+    private const string TestData = "";
+
+    public Day01Tests(Day01 sut)
+    {
+        _sut = sut;
+    }
+
+    [Theory(DisplayName = "Part1 should return expected results from example data")]
+    [InlineData("", 0)]
+    public void Part1Example(string input, int expected)
+    {
+        _sut.Part1(input).As<int>().Should().Be(expected);
+    }
+
+
+    [Theory(DisplayName = "Part2 should return expected results from example data")]
+    [InlineData("", 0)]
+    public void Part2Example(string input, int expected)
+    {
+        _sut.Part2(input).As<int>().Should().Be(expected);
+    }
+}
