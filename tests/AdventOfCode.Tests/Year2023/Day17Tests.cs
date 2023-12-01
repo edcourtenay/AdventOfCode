19a6c155f1abfe259e04ff305ac4dffacffffc14
Ed Courtenay <ed@edcourtenay.co.uk>
2023-12-01 15:09:02 +0000

    Skeleton

---
+++ tests/AdventOfCode.Tests/Year2023/Day17Tests.cs
@@ -0,0 +1,32 @@
+using AdventOfCode.Year2023;
+
+namespace AdventOfCode.Tests.Year2023;
+
+public class Day17Tests : IClassFixture<Day17>
+{
+    private readonly Day17 _sut;
+
+    private const string TestData =
+        """
+
+        """;
+
+    public Day17Tests(Day17 sut)
+    {
+        _sut = sut;
+    }
+
+    [Theory(DisplayName = "Part1 should return expected results from example data")]
+    [InlineData(TestData, -1)]
+    public void Part1Example(string input, int expected)
+    {
+        _sut.Part1(input).As<int>().Should().Be(expected);
+    }
+
+    [Theory(DisplayName = "Part2 should return expected results from example data")]
+    [InlineData(TestData, -1)]
+    public void Part2Example(string input, int expected)
+    {
+        _sut.Part2(input).As<int>().Should().Be(expected);
+    }
+}
