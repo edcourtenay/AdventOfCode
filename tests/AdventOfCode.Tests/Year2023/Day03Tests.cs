72a9a98872b63140cfdc67e29f0ff23514faa467
Ed Courtenay <ed@edcourtenay.co.uk>
2023-12-01 14:44:40 +0000

    2023-03: Gear Ratios

---
+++ tests/AdventOfCode.Tests/Year2023/Day03Tests.cs
@@ -0,0 +1,41 @@
+using AdventOfCode.Year2023;
+
+namespace AdventOfCode.Tests.Year2023;
+
+public class Day03Tests : IClassFixture<Day03>
+{
+    private readonly Day03 _sut;
+
+    private const string TestData =
+        """
+        467..114..
+        ...*......
+        ..35..633.
+        ......#...
+        617*......
+        .....+.58.
+        ..592.....
+        ......755.
+        ...$.*....
+        .664.598..
+        """;
+
+    public Day03Tests(Day03 sut)
+    {
+        _sut = sut;
+    }
+
+    [Theory(DisplayName = "Part1 should return expected results from example data")]
+    [InlineData(TestData, 4361)]
+    public void Part1Example(string input, int expected)
+    {
+        _sut.Part1(input).As<int>().Should().Be(expected);
+    }
+
+    [Theory(DisplayName = "Part2 should return expected results from example data")]
+    [InlineData(TestData, 467835)]
+    public void Part2Example(string input, int expected)
+    {
+        _sut.Part2(input).As<int>().Should().Be(expected);
+    }
+}
