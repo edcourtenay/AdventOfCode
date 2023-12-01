eba57f39d78c2b53afda1886af55baecd5a5b8a1
Ed Courtenay <ed@edcourtenay.co.uk>
2023-12-01 15:11:01 +0000

    Skeleton

---
+++ src/AdventOfCode/Year2023/Day20.cs
@@ -0,0 +1,15 @@
+namespace AdventOfCode.Year2023;
+
+[Description("Not set...")]
+public class Day20 : IPuzzle
+{
+    public object Part1(string input)
+    {
+        throw new NotImplementedException();
+    }
+
+    public object Part2(string input)
+    {
+        throw new NotImplementedException();
+    }
+}
