51fc6d7b75d70d2ce2f941d1af4e5c2f9c4592e8
Ed Courtenay <ed@edcourtenay.co.uk>
2023-12-01 09:05:54 +0000

    Complete

--- src/AdventOfCode/Year2023/Day01.cs
+++ src/AdventOfCode/Year2023/Day01.cs
@@ -1,8 +1,58 @@
-﻿namespace AdventOfCode.Year2023;
+﻿using System.Text.RegularExpressions;
 
-public class Day01 : IPuzzle
+namespace AdventOfCode.Year2023;
+
+[Description("Trebuchet?!")]
+public partial class Day01 : IPuzzle
 {
-    public object Part1(string input) => string.Empty;
+    private const string NumbersExpression = "zero|one|two|three|four|five|six|seven|eight|nine|\\d";
 
-    public object Part2(string input) => string.Empty;
+    [GeneratedRegex(NumbersExpression, RegexOptions.Compiled )]
+    private static partial Regex LeftNumbersRegex();
+
+    [GeneratedRegex(NumbersExpression, RegexOptions.Compiled | RegexOptions.RightToLeft )]
+    private static partial Regex RightNumbersRegex();
+
+    public object Part1(string input)
+    {
+        return input.ToLines(ParseLine)
+            .Sum();
+    }
+
+    public object Part2(string input)
+    {
+        return input.ToLines(ParseLine2)
+            .Sum();
+    }
+
+    private int ParseLine(string line)
+    {
+        return (line.First(char.IsAsciiDigit) - '0') * 10 + (line.Last(char.IsAsciiDigit) - '0');
+    }
+
+    private int ParseLine2(string line)
+    {
+        Match first = LeftNumbersRegex().Match(line);
+        Match second = RightNumbersRegex().Match(line);
+
+        return MatchToInt(first) * 10 + MatchToInt(second);
+    }
+
+    private int MatchToInt(Match match)
+    {
+        return match.Value switch
+        {
+            "zero" or "0" => 0,
+            "one" or "1" => 1,
+            "two" or "2" => 2,
+            "three" or "3" => 3,
+            "four" or "4" => 4,
+            "five" or "5" => 5,
+            "six" or "6" => 6,
+            "seven" or "7" => 7,
+            "eight" or "8" => 8,
+            "nine" or "9" => 9,
+            _ => throw new ArgumentOutOfRangeException()
+        };
+    }
 }
--- tests/AdventOfCode.Tests/Year2023/Day01Tests.cs
+++ tests/AdventOfCode.Tests/Year2023/Day01Tests.cs
@@ -1,4 +1,4 @@
-﻿using AdventOfCode.Year2022;
+﻿using AdventOfCode.Year2023;
 
 namespace AdventOfCode.Tests.Year2023;
 
@@ -6,7 +6,22 @@ public class Day01Tests : IClassFixture<Day01>
 {
     private readonly Day01 _sut;
 
-    private const string TestData = "";
+    private const string Day01TestData = """
+                                    1abc2
+                                    pqr3stu8vwx
+                                    a1b2c3d4e5f
+                                    treb7uchet
+                                    """;
+
+    private const string Day02TestData = """
+                                         two1nine
+                                         eightwothree
+                                         abcone2threexyz
+                                         xtwone3four
+                                         4nineeightseven2
+                                         zoneight234
+                                         7pqrstsixteen
+                                         """;
 
     public Day01Tests(Day01 sut)
     {
@@ -14,7 +29,7 @@ public class Day01Tests : IClassFixture<Day01>
     }
 
     [Theory(DisplayName = "Part1 should return expected results from example data")]
-    [InlineData("", 0)]
+    [InlineData(Day01TestData, 142)]
     public void Part1Example(string input, int expected)
     {
         _sut.Part1(input).As<int>().Should().Be(expected);
@@ -22,9 +37,11 @@ public class Day01Tests : IClassFixture<Day01>
 
 
     [Theory(DisplayName = "Part2 should return expected results from example data")]
-    [InlineData("", 0)]
+    [InlineData(Day02TestData, 281)]
     public void Part2Example(string input, int expected)
     {
         _sut.Part2(input).As<int>().Should().Be(expected);
     }
+
+
 }
