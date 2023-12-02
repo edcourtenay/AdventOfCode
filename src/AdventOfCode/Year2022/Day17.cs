c1da414e891ae4ebf3edf8081f4d2c114d4beaf3
Ed Courtenay <ed@edcourtenay.co.uk>
2023-12-02 01:12:07 +0000

    WIP

---
+++ src/AdventOfCode/Year2022/Day17.cs
@@ -0,0 +1,157 @@
+﻿using System.Diagnostics;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace AdventOfCode.Year2022;
+
+[Description("Pyroclastic Flow")]
+public class Day17 : IPuzzle
+{
+    private const int LeftMask  = 0b100000;
+    private const int RightMask = 0b000001;
+
+    private static readonly Move LeftMove = new Move(LeftMask, r => r << 1);
+    private static readonly Move RightMove = new Move(RightMask, r => r >> 1);
+
+
+    private static readonly int[][] RockData = {
+        new[] { 0b0011110 },
+        new[] { 0b0001000, 0b0011100, 0b0001000 },
+        new[] { 0b0000100, 0b0000100, 0b0011100 },
+        new[] { 0b0010000, 0b0010000, 0b0010000, 0b0010000 },
+        new[] { 0b0011000, 0b0011000 }
+    };
+
+    public object Part1(string input)
+    {
+        return Solve(input.ToCharArray(), 2022);
+    }
+
+    public object Part2(string input)
+    {
+        return Solve(input.ToCharArray(), 1000000000000);
+    }
+
+    private static long Solve(char[] wind, long iterations)
+    {
+        LinkedListNode<char> currentWind = new LinkedList<char>(wind).First!;
+        Dictionary<int, int> chamber = new()
+        {
+            { 0, 0b1111111 }
+        };
+
+        var rockIndex = 0;
+        var rock = GetRock(ref rockIndex);
+        var rockRow = TopRow(chamber) + rock.Length + 3;
+
+        for (long i = 0; i < iterations; )
+        {
+            Visualise(rock, rockRow, chamber);
+
+            rock = Blow(rock, rockRow, currentWind.Value, chamber);
+            currentWind = currentWind.NextOrFirst();
+
+            if (CanMove(rock, rockRow - 1, chamber))
+            {
+                rockRow--;
+            }
+            else
+            {
+                for (int j = 0; j < rock.Length; j++)
+                {
+                    int chamberRow = rockRow + rock.Length - j - 1;
+                    var row = chamber.TryGetValue(chamberRow, out var r) ? r : 0;
+                    chamber[chamberRow] = row | rock[j];
+                }
+
+                i++;
+                rock = GetRock(ref rockIndex);
+                rockRow = TopRow(chamber) + rock.Length + 3;
+            }
+        }
+
+        return TopRow(chamber);
+    }
+
+    private static void Visualise(int[] rock, int rockRow, Dictionary<int,int> chamber)
+    {
+        var sb = new StringBuilder();
+        for (int i = rockRow; i > 0; i--)
+        {
+            sb.Clear();
+            sb.Append("|       |");
+            int mask = 0b1000000;
+            int j = 1;
+            while (mask > 0)
+            {
+                if ((chamber.TryGetValue(i, out int value) ? value : 0 & mask) == 0)
+                {
+                    sb[j] = '#';
+                }
+                j++;
+                mask = mask >> 1;
+            }
+            Console.WriteLine(sb.ToString());
+        }
+        Console.WriteLine("+-------+");
+        Console.WriteLine();
+    }
+
+    private static int TopRow(Dictionary<int, int> chamber)
+    {
+        int fullRow = chamber.Where(pair => pair.Value == 0b1111111).Select(pair => pair.Key).DefaultIfEmpty(0).Max();
+        foreach (var key in chamber.Keys.Where(k => k < fullRow))
+        {
+            chamber.Remove(key);
+        }
+
+        return chamber.Where(pair => pair.Value != 0).Select(pair => pair.Key).DefaultIfEmpty(0).Max();
+    }
+
+    private static int[] GetRock(ref int rockIndex)
+    {
+        int index = rockIndex;
+        rockIndex = (rockIndex + 1) % RockData.Length;
+        return RockData[index].Select(r => r).ToArray();
+    }
+
+    private static bool CanRotate(IEnumerable<int> rock, int mask)
+    {
+        return rock.All(r => (r & mask) == 0);
+    }
+
+    private static int[] Blow(int[] rock, int rockRow, char direction, Dictionary<int, int> chamber)
+    {
+        (int mask, Func<int, int> func) = direction switch
+        {
+            '<' => LeftMove,
+            '>' => RightMove,
+            _ => throw new ArgumentOutOfRangeException(nameof(direction))
+        };
+
+        if (!CanRotate(rock, mask))
+        {
+            return rock;
+        }
+
+        var newRock = rock.Select(r => func(r)).ToArray();
+
+        return CanMove(newRock, rockRow, chamber)
+            ? newRock
+            : rock;
+    }
+
+    private static bool CanMove(int[] newRock, int rockRow, Dictionary<int, int> chamber)
+    {
+        var canMove = true;
+        for (int i = 0; i < newRock.Length; i++)
+        {
+            var row = chamber.TryGetValue(rockRow + newRock.Length - i - 1, out var r) ? r : 0;
+            canMove &= (newRock[i] & row) == 0;
+        }
+
+        return canMove;
+    }
+
+    record Move(int Mask, Func<int, int> Func);
+}
---
+++ tests/AdventOfCode.Tests/Year2022/Day17Tests.cs
@@ -0,0 +1,29 @@
+﻿using AdventOfCode.Year2022;
+
+namespace AdventOfCode.Tests.Year2022;
+
+public class Day17Tests : IClassFixture<Day17>
+{
+    private readonly Day17 _sut;
+
+    private const string TestData = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>";
+
+    public Day17Tests(Day17 sut)
+    {
+        _sut = sut;
+    }
+
+    [Theory(DisplayName = "Part1 should return expected results from example data")]
+    [InlineData(TestData, 3068)]
+    public void Part1Example(string input, long expected)
+    {
+        _sut.Part1(input).As<long>().Should().Be(expected);
+    }
+
+    [Theory(DisplayName = "Part2 should return expected results from example data")]
+    [InlineData(TestData, 1514285714288)]
+    public void Part2Example(string input, long expected)
+    {
+        _sut.Part2(input).As<long>().Should().Be(expected);
+    }
+}
