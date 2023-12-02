da0490664487c50737679e10f5d3bbb0c488b0d1
Ed Courtenay <ed@edcourtenay.co.uk>
2023-12-02 17:21:58 +0000

    Completed 2023 Day 02
    2023 Day 02: Cube Conundrum
            Part 1: [    326.90µs] 2331 V
            Part 2: [    243.40µs] 71585 V

--- tests/AdventOfCode.Tests/Year2023/Day02Tests.cs
+++ tests/AdventOfCode.Tests/Year2023/Day02Tests.cs
@@ -8,7 +8,11 @@ public class Day02Tests : IClassFixture<Day02>
 
     private const string TestData =
         """
-
+        Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+        Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+        Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+        Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+        Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
         """;
 
     public Day02Tests(Day02 sut)
@@ -17,16 +21,29 @@ public class Day02Tests : IClassFixture<Day02>
     }
 
     [Theory(DisplayName = "Part1 should return expected results from example data")]
-    [InlineData(TestData, -1)]
+    [InlineData(TestData, 8)]
     public void Part1Example(string input, int expected)
     {
         _sut.Part1(input).As<int>().Should().Be(expected);
     }
 
     [Theory(DisplayName = "Part2 should return expected results from example data")]
-    [InlineData(TestData, -1)]
+    [InlineData(TestData, 2286)]
     public void Part2Example(string input, int expected)
     {
         _sut.Part2(input).As<int>().Should().Be(expected);
+    }
+
+    [Fact(DisplayName = "ParseGame should return expected results from example data")]
+    public void ParseGameTest()
+    {
+        var result = _sut.ParseGame("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red");
+        result.Should().BeEquivalentTo(new Day02.Game(3,
+            new[]
+            {
+                new Day02.Set(20, 8, 6),
+                new Day02.Set(4, 13, 5),
+                new Day02.Set(1, 5, 0)
+            }));
     }
 }
