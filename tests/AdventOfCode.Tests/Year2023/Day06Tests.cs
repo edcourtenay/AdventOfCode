0c483379e3fb846e7771f5fb2fa389c55d4c51fa
Ed Courtenay <ed@edcourtenay.co.uk>
2023-12-06 17:03:22 +0000

    2023-06: Wait For It

--- tests/AdventOfCode.Tests/Year2023/Day06Tests.cs
+++ tests/AdventOfCode.Tests/Year2023/Day06Tests.cs
@@ -8,7 +8,8 @@ public class Day06Tests : IClassFixture<Day06>
 
     private const string TestData =
         """
-
+        Time:      7  15   30
+        Distance:  9  40  200
         """;
 
     public Day06Tests(Day06 sut)
@@ -17,16 +18,25 @@ public class Day06Tests : IClassFixture<Day06>
     }
 
     [Theory(DisplayName = "Part1 should return expected results from example data")]
-    [InlineData(TestData, -1)]
+    [InlineData(TestData, 288)]
     public void Part1Example(string input, int expected)
     {
         _sut.Part1(input).As<int>().Should().Be(expected);
     }
 
     [Theory(DisplayName = "Part2 should return expected results from example data")]
-    [InlineData(TestData, -1)]
+    [InlineData(TestData, 71503)]
     public void Part2Example(string input, int expected)
     {
         _sut.Part2(input).As<int>().Should().Be(expected);
+    }
+
+    [Fact(DisplayName = "Parse should return expected results from example data")]
+    public void ParseInputExample()
+    {
+        _sut.Parse(TestData).Should().BeEquivalentTo(new Day06.Race[]
+        {
+            new (9, 7), new (40, 15), new (200, 30)
+        });
     }
 }
