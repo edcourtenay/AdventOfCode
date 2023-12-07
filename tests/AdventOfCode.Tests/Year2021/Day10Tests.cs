17198509907236c60f51bd9665f09d2d949d478e
Ed Courtenay <ed@edcourtenay.co.uk>
2023-12-07 23:20:27 +0000

    2021-10: Syntax Scoring
    ```
    2021 Day 10: Syntax Scoring
            Part 1: [      5.14ms] 389589 √
            Part 2: [      4.37ms] 1190420163 √
    ```

---
+++ tests/AdventOfCode.Tests/Year2021/Day10Tests.cs
@@ -0,0 +1,55 @@
+﻿using AdventOfCode.Year2021;
+
+namespace AdventOfCode.Tests.Year2021;
+
+public class Day10Tests : IClassFixture<Day10>
+{
+    private readonly Day10 _sut;
+
+    private const string TestData =
+        """
+        [({(<(())[]>[[{[]{<()<>>
+        [(()[<>])]({[<{<<[]>>(
+        {([(<{}[<>[]}>{[]{[(<()>
+        (((({<>}<{<{<>}{[]{[]{}
+        [[<[([]))<([[{}[[()]]]
+        [{[{({}]{}}([{[{{{}}([]
+        {<[[]]>}<{[{[{[]{()[[[]
+        [<(<(<(<{}))><([]([]()
+        <{([([[(<>()){}]>(<<{{
+        <{([{{}}[<[[[<>{}]]]>[]]
+        """;
+
+    public Day10Tests(Day10 sut)
+    {
+        _sut = sut;
+    }
+
+    [Fact(DisplayName = "Part1 should return expected results from example data")]
+    public void Part1Example()
+    {
+        _sut.Part1(TestData).As<long>().Should().Be(26397);
+    }
+
+    [Fact(DisplayName = "Part2 should return expected results from example data")]
+    public void Part2Example()
+    {
+        _sut.Part2(TestData).As<long>().Should().Be(288957);
+    }
+
+    [Theory(DisplayName = "IsCorrupted should return expected results from example data")]
+    [InlineData(@"[({(<(())[]>[[{[]{<()<>>", false, '\0', "}}]])})]")]
+    [InlineData(@"[(()[<>])]({[<{<<[]>>(", false, '\0', ")}>]})")]
+    [InlineData(@"{([(<{}[<>[]}>{[]{[(<()>", true, '}', "")]
+    [InlineData(@"(((({<>}<{<{<>}{[]{[]{}", false, '\0', "}}>}>))))")]
+    [InlineData(@"[[<[([]))<([[{}[[()]]]", true, ')', "")]
+    [InlineData(@"[{[{({}]{}}([{[{{{}}([]", true, ']', "")]
+    [InlineData(@"{<[[]]>}<{[{[{[]{()[[[]", false, '\0', "]]}}]}]}>")]
+    [InlineData(@"[<(<(<(<{}))><([]([]()", true, ')', "")]
+    [InlineData(@"<{([([[(<>()){}]>(<<{{", true, '>', "")]
+    [InlineData(@"<{([{{}}[<[[[<>{}]]]>[]]", false, '\0', "])}>")]
+    public void IsCorruptedExample(string input, bool isCorrupted, char corrupt, string remainder)
+    {
+        _sut.Validate(input).Should().Be((isCorrupted, corrupt, remainder));
+    }
+}
