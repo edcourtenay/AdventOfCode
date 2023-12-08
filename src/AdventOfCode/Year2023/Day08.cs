c7721578c668ff6e4200e3b99a94ca952ad1d5f5
Ed Courtenay <ed@edcourtenay.co.uk>
2023-12-08 12:53:09 +0000

    2023-08: Haunted Wasteland

--- src/AdventOfCode/Year2023/Day08.cs
+++ src/AdventOfCode/Year2023/Day08.cs
@@ -1,15 +1,130 @@
+using System.Numerics;
+
 namespace AdventOfCode.Year2023;
 
-[Description("Not set...")]
+[Description("Haunted Wasteland")]
 public class Day08 : IPuzzle
 {
     public object Part1(string input)
     {
-        throw new NotImplementedException();
+        var map = Map.Parse(input);
+        return map.TraverseFrom("AAA")
+            .Select((s, i) => (Location: s, Step: i))
+            .Where(tuple => tuple.Location == "ZZZ")
+            .Select(tuple => tuple.Step)
+            .First();
     }
 
     public object Part2(string input)
     {
-        throw new NotImplementedException();
+        var map = Map.Parse(input);
+
+        var steps = map.StartNodes
+            .Select(map.CycleLength)
+            .Order()
+            .ToArray();
+
+
+        return LeastCommonMultiple(steps);
+    }
+
+    private static (string node, string left, string right) ParseInstruction(string line)
+    {
+        return (line[..3], line[7..10], line[12..15]);
+    }
+
+    static T LeastCommonMultiple<T>(IEnumerable<T> numbers) where T : INumber<T>
+    {
+        return numbers.Aggregate(LeastCommonMultiple);
+    }
+    static T LeastCommonMultiple<T>(T a, T b) where T : INumber<T>
+    {
+        return T.Abs(a * b) / GreatestCommonDivisor(a, b);
+    }
+
+    static T GreatestCommonDivisor<T>(T a, T b) where T : INumber<T>
+    {
+        while (true)
+        {
+            if (b == T.Zero)
+            {
+                return a;
+            }
+
+            var temp = a;
+            a = b;
+            b = temp % b;
+        }
+    }
+
+    public class Map
+    {
+        private readonly LinkedList<char> _directionList;
+        private readonly IDictionary<string, (string Left, string Right)> _map;
+
+        private Map(LinkedList<char> directionList, IDictionary<string, (string Left, string Right)> map, string[] startNodes)
+        {
+            _directionList = directionList;
+            _map = map;
+            StartNodes = startNodes;
+        }
+
+        public static Map Parse(string input)
+        {
+            var lines = input.ToLines().ToArray();
+            var map = new Dictionary<string, (string Left, string Right)>();
+            var directionList = new LinkedList<char>(lines[0]);
+            foreach (var line in lines[2..])
+            {
+                (string node, string left, string right) = ParseInstruction(line);
+                map.Add(node, (left, right));
+            }
+
+            var startNodes = map
+                .Where(kvp => kvp.Key is [.., 'A'])
+                .Select(kvp => kvp.Key)
+                .ToArray();
+
+
+            return new Map(directionList, map, startNodes);
+        }
+
+        public string[] StartNodes { get; }
+
+        public IEnumerable<string> TraverseFrom(string start)
+        {
+            var direction = _directionList.First!;
+            var node = start;
+            while (true)
+            {
+                yield return node;
+                (string left, string right) = _map[node];
+                node = direction.Value switch
+                {
+                    'L' => left,
+                    'R' => right,
+                    _ => throw new InvalidOperationException()
+                };
+                direction = direction.NextOrFirst();
+            }
+        }
+
+        public long CycleLength(string start)
+        {
+            using IEnumerator<string> enumerator = TraverseFrom(start).GetEnumerator();
+
+            var set = new HashSet<(string, int)>();
+            var current = (Node: string.Empty, Step: -1);
+            var length = _directionList.Count;
+
+            enumerator.MoveNext();
+
+            do
+            {
+                current = (enumerator.Current, (current.Step + 1) % length);
+            } while (set.Add(current) && enumerator.MoveNext());
+
+            return set.Count - current.Step;
+        }
     }
 }
--- tests/AdventOfCode.Tests/Year2023/Day08Tests.cs
+++ tests/AdventOfCode.Tests/Year2023/Day08Tests.cs
@@ -6,9 +6,40 @@ public class Day08Tests : IClassFixture<Day08>
 {
     private readonly Day08 _sut;
 
-    private const string TestData =
+    private const string TestData1=
         """
+        RL
+        
+        AAA = (BBB, CCC)
+        BBB = (DDD, EEE)
+        CCC = (ZZZ, GGG)
+        DDD = (DDD, DDD)
+        EEE = (EEE, EEE)
+        GGG = (GGG, GGG)
+        ZZZ = (ZZZ, ZZZ)
+        """;
 
+    private const string TestData2 =
+        """
+        LLR
+        
+        AAA = (BBB, BBB)
+        BBB = (AAA, ZZZ)
+        ZZZ = (ZZZ, ZZZ)
+        """;
+
+    private const string TestData3 =
+        """
+        LR
+
+        11A = (11B, XXX)
+        11B = (XXX, 11Z)
+        11Z = (11B, XXX)
+        22A = (22B, XXX)
+        22B = (22C, 22C)
+        22C = (22Z, 22Z)
+        22Z = (22B, 22B)
+        XXX = (XXX, XXX)
         """;
 
     public Day08Tests(Day08 sut)
@@ -17,16 +48,17 @@ public class Day08Tests : IClassFixture<Day08>
     }
 
     [Theory(DisplayName = "Part1 should return expected results from example data")]
-    [InlineData(TestData, -1)]
+    [InlineData(TestData1, 2)]
+    [InlineData(TestData2, 6)]
     public void Part1Example(string input, int expected)
     {
         _sut.Part1(input).As<int>().Should().Be(expected);
     }
 
     [Theory(DisplayName = "Part2 should return expected results from example data")]
-    [InlineData(TestData, -1)]
+    [InlineData(TestData3, 6)]
     public void Part2Example(string input, int expected)
     {
-        _sut.Part2(input).As<int>().Should().Be(expected);
+        _sut.Part2(input).As<long>().Should().Be(expected);
     }
 }
