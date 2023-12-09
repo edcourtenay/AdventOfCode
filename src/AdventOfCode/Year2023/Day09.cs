69ee82d37d7a0327b3c16094b66058c21d0e088f
Ed Courtenay <ed@edcourtenay.co.uk>
2023-12-09 09:37:19 +0000

    2023-09: Mirage Maintenance

--- src/AdventOfCode/Year2023/Day09.cs
+++ src/AdventOfCode/Year2023/Day09.cs
@@ -1,15 +1,49 @@
+using System.Numerics;
+
 namespace AdventOfCode.Year2023;
 
-[Description("Not set...")]
+[Description("Mirage Maintenance")]
 public class Day09 : IPuzzle
 {
     public object Part1(string input)
     {
-        throw new NotImplementedException();
+        return Solve(input, tuple => tuple.right);
     }
 
     public object Part2(string input)
     {
-        throw new NotImplementedException();
+        return Solve(input, tuple => tuple.left);
+    }
+
+    private long Solve(string input, Func<(long left, long right), long> selector)
+    {
+        return input.ToLines(line => line.Split(' ').Select(long.Parse).ToArray())
+            .Select(Extrapolate)
+            .Sum(selector);
+    }
+
+    public T[] GenerateSequence<T>(IEnumerable<T> input) where T : INumber<T> =>
+        input.SlidingWindow(2).Select(window => window.ToArray()).Select(w => w[1] - w[0]).ToArray();
+
+    public (T left, T right) Extrapolate<T>(T[] input) where T : INumber<T>
+    {
+        var leftStack = new Stack<T>();
+        var rightStack = new Stack<T>();
+
+        while (input.Any(t => !T.IsZero(t)))
+        {
+            leftStack.Push(input[0]);
+            rightStack.Push(input[^1]);
+            input = GenerateSequence(input);
+        }
+        var left = T.Zero;
+        var right = T.Zero;
+        while (rightStack.TryPop(out T? rightCurrent) && leftStack.TryPop(out T? leftCurrent))
+        {
+            left = leftCurrent - left;
+            right += rightCurrent;
+        }
+
+        return (left, right);
     }
 }
--- tests/AdventOfCode.Tests/Year2023/Day09Tests.cs
+++ tests/AdventOfCode.Tests/Year2023/Day09Tests.cs
@@ -8,7 +8,9 @@ public class Day09Tests : IClassFixture<Day09>
 
     private const string TestData =
         """
-
+        0 3 6 9 12 15
+        1 3 6 10 15 21
+        10 13 16 21 30 45
         """;
 
     public Day09Tests(Day09 sut)
@@ -17,16 +19,33 @@ public class Day09Tests : IClassFixture<Day09>
     }
 
     [Theory(DisplayName = "Part1 should return expected results from example data")]
-    [InlineData(TestData, -1)]
-    public void Part1Example(string input, int expected)
+    [InlineData(TestData, 114)]
+    public void Part1Example(string input, long expected)
     {
-        _sut.Part1(input).As<int>().Should().Be(expected);
+        _sut.Part1(input).As<long>().Should().Be(expected);
     }
 
     [Theory(DisplayName = "Part2 should return expected results from example data")]
-    [InlineData(TestData, -1)]
-    public void Part2Example(string input, int expected)
+    [InlineData(TestData, 2)]
+    public void Part2Example(string input, long expected)
     {
-        _sut.Part2(input).As<int>().Should().Be(expected);
+        _sut.Part2(input).As<long>().Should().Be(expected);
+    }
+
+    [Theory(DisplayName = "GenerateSequence should return expected results from puzzle input")]
+    [InlineData(new long[] { 0, 3, 6, 9, 12, 15}, new long[] { 3, 3, 3, 3, 3})]
+    [InlineData(new long[] { 3, 3, 3, 3, 3}, new long[] { 0, 0, 0, 0 })]
+    public void GenerateExamples(long[] input, long[] expected)
+    {
+        _sut.GenerateSequence(input).Should().BeEquivalentTo(expected);
+    }
+
+    [Theory(DisplayName = "Extrapolate should return expected results from puzzle input")]
+    [InlineData(new long[] { 0, 3, 6, 9, 12, 15 }, -3, 18)]
+    [InlineData(new long[] { 1, 3, 6, 10, 15, 21 }, 0, 28)]
+    [InlineData(new long[] { 10, 13, 16, 21, 30, 45 }, 5, 68)]
+    public void ExtrapolateExamples(long[] input, long left, long right)
+    {
+        _sut.Extrapolate(input).Should().Be((left, right));
     }
 }
