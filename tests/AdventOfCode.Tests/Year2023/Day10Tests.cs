4e9a0731fe9eac8b31bbf94a961c1841df23cf6a
Ed Courtenay <ed@edcourtenay.co.uk>
2023-12-10 22:35:22 +0000

    2023-10: Pipe Maze

--- tests/AdventOfCode.Tests/Year2023/Day10Tests.cs
+++ tests/AdventOfCode.Tests/Year2023/Day10Tests.cs
@@ -1,3 +1,5 @@
+using System.Runtime.InteropServices;
+
 using AdventOfCode.Year2023;
 
 namespace AdventOfCode.Tests.Year2023;
@@ -6,9 +8,27 @@ public class Day10Tests : IClassFixture<Day10>
 {
     private readonly Day10 _sut;
 
-    private const string TestData =
+    private const string Part1TestData =
         """
+        7-F7-
+        .FJ|7
+        SJLL7
+        |F--J
+        LJ.LJ
+        """;
 
+    private const string Part2TestData =
+        """
+        FF7FSF7F7F7F7F7F---7
+        L|LJ||||||||||||F--J
+        FL-7LJLJ||||||LJL-77
+        F--JF--7||LJLJ7F7FJ-
+        L---JF-JLJ.||-FJLJJ7
+        |F|F-JF---7F7-L7L|7|
+        |FFJF7L7F-JF7|JL---7
+        7-L-JL7||F7|L7F-7F7|
+        L.L7LFJ|||||FJL7||LJ
+        L7JLJL-JLJLJL--JLJ.L
         """;
 
     public Day10Tests(Day10 sut)
@@ -17,14 +37,14 @@ public class Day10Tests : IClassFixture<Day10>
     }
 
     [Theory(DisplayName = "Part1 should return expected results from example data")]
-    [InlineData(TestData, -1)]
+    [InlineData(Part1TestData, 8)]
     public void Part1Example(string input, int expected)
     {
         _sut.Part1(input).As<int>().Should().Be(expected);
     }
 
     [Theory(DisplayName = "Part2 should return expected results from example data")]
-    [InlineData(TestData, -1)]
+    [InlineData(Part2TestData, 10)]
     public void Part2Example(string input, int expected)
     {
         _sut.Part2(input).As<int>().Should().Be(expected);
