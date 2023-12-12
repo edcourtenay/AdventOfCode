458b28dfd08be13c880f5cc672adba3bf076cf68
Ed Courtenay <ed@edcourtenay.co.uk>
2023-12-12 09:39:14 +0000

    2023-11: Cosmic Expansion

--- src/AdventOfCode/Year2023/Day11.cs
+++ src/AdventOfCode/Year2023/Day11.cs
@@ -1,15 +1,74 @@
+using System.Numerics;
+
+using Point = (long X, long Y);
+
 namespace AdventOfCode.Year2023;
 
-[Description("Not set...")]
+[Description("Cosmic Expansion")]
 public class Day11 : IPuzzle
 {
     public object Part1(string input)
     {
-        throw new NotImplementedException();
+        return Solve(input, 2L);
     }
 
     public object Part2(string input)
     {
-        throw new NotImplementedException();
+        return Solve(input, 1000000L);
+    }
+
+    public static long Solve(string input, long expansionFactor)
+    {
+        return Expand(Parse(input), expansionFactor).ToArray()
+            .Combinations(2)
+            .Select(tuples => tuples.ToArray())
+            .Sum(tuples => GetManhattanDistance(tuples[0], tuples[1]));
+    }
+
+    public static IEnumerable<Point> Parse(string input)
+    {
+        return input.ToLines()
+            .Select((line, y) => (line, y))
+            .SelectMany(t => t.line.Select((c, x) => (c, x, t.y)).Where(t => t.c == '#'))
+            .Select(t => ((long)t.x, (long)t.y));
+    }
+
+    public static IEnumerable<Point> Expand(IEnumerable<Point> source, long expansionFactor)
+    {
+        List<Point> points = source.ToList();
+        (long minX, long maxX) = points.Select(p => p.X).MinMax();
+        (long minY, long maxY) = points.Select(p => p.Y).MinMax();
+
+        return ExpandAxis(
+            ExpandAxis(points, minX, maxX, (point, i) => point.X == i, (point, i) => (point.X + i, point.Y), expansionFactor), minY,
+            maxY, (point, i) => point.Y == i, (point, i) => (point.X, point.Y + i), expansionFactor);
+    }
+
+    private static IEnumerable<Point> ExpandAxis(IEnumerable<Point> source, long min, long max, Func<Point, long, bool> predicate, Func<Point, long, Point> generator, long expansionFactor)
+    {
+        var points = source.ToList();
+
+        var expansion = 0L;
+        for (long i = min; i <= max; i++)
+        {
+            var j = i;
+            var set = points.Where(p => predicate(p, j)).ToArray();
+
+            if (set.Length == 0)
+            {
+                expansion += (expansionFactor - 1);
+                continue;
+            }
+
+            foreach (Point point in set)
+            {
+                yield return generator(point, expansion);
+            }
+        }
+    }
+
+    private static T GetManhattanDistance<T>((T x, T y) p1, (T x, T y) p2) where T : INumber<T>
+    {
+        return T.Abs(p1.x - p2.x) + T.Abs(p1.y - p2.y);
     }
 }
--- tests/AdventOfCode.Tests/Year2023/Day11Tests.cs
+++ tests/AdventOfCode.Tests/Year2023/Day11Tests.cs
@@ -8,7 +8,16 @@ public class Day11Tests : IClassFixture<Day11>
 
     private const string TestData =
         """
-
+        ...#......
+        .......#..
+        #.........
+        ..........
+        ......#...
+        .#........
+        .........#
+        ..........
+        .......#..
+        #...#.....
         """;
 
     public Day11Tests(Day11 sut)
@@ -17,16 +26,32 @@ public class Day11Tests : IClassFixture<Day11>
     }
 
     [Theory(DisplayName = "Part1 should return expected results from example data")]
-    [InlineData(TestData, -1)]
-    public void Part1Example(string input, int expected)
+    [InlineData(TestData, 2, 374)]
+    [InlineData(TestData, 10, 1030)]
+    [InlineData(TestData, 100, 8410)]
+    public void SolveExample(string input, long expansionFactor, long expected)
     {
-        _sut.Part1(input).As<int>().Should().Be(expected);
+        Day11.Solve(input, expansionFactor).As<long>().Should().Be(expected);
     }
 
-    [Theory(DisplayName = "Part2 should return expected results from example data")]
-    [InlineData(TestData, -1)]
-    public void Part2Example(string input, int expected)
+    [Fact(DisplayName = "Parse should return expected results from example data")]
+    public void ParseTest()
     {
-        _sut.Part2(input).As<int>().Should().Be(expected);
+        Day11.Parse(TestData).Should().BeEquivalentTo(new[]
+        {
+            (3, 0), (7, 1), (0, 2), (6, 4), (1, 5), (9, 6), (7, 8), (0, 9), (4, 9)
+        });
+    }
+
+    [Fact(DisplayName = "Expand should return expected results from example data")]
+    public void ExpandTest()
+    {
+        Day11.Expand(new (long, long)[]
+        {
+            (3, 0), (7, 1), (0, 2), (6, 4), (1, 5), (9, 6), (7, 8), (0, 9), (4, 9)
+        }, 2L).Should().BeEquivalentTo(new[]
+        {
+            (4, 0), (9, 1), (0, 2), (8, 5), (1, 6), (12, 7), (9, 10), (0, 11), (5, 11)
+        });
     }
 }
