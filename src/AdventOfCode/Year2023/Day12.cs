c447ca74af4240ebbc92e30be918512e9c5f5c4f
Ed Courtenay <ed@edcourtenay.co.uk>
2023-12-12 16:18:37 +0000

    2023-12: Hot Springs

--- src/AdventOfCode/Year2023/Day12.cs
+++ src/AdventOfCode/Year2023/Day12.cs
@@ -1,15 +1,69 @@
+using LineData = (string input, int[] springLengths);
+
 namespace AdventOfCode.Year2023;
 
-[Description("Not set...")]
+[Description("Hot Springs")]
 public class Day12 : IPuzzle
 {
     public object Part1(string input)
     {
-        throw new NotImplementedException();
+        return input.ToLines(Parse)
+            .AsParallel()
+            .Sum(data => CalculateLegalPositions(data.input, data.springLengths));
     }
 
     public object Part2(string input)
     {
-        throw new NotImplementedException();
+        return input.ToLines(Parse)
+            .AsParallel()
+            .Select(Unfold)
+            .Sum(data => CalculateLegalPositions(data.input, data.springLengths));
+    }
+
+    private LineData Unfold(LineData data) =>
+        (string.Join('?', Enumerable.Repeat(data.input, 5)), Enumerable.Repeat(data.springLengths, 5).SelectMany(x => x).ToArray());
+
+    private static LineData Parse(string line)
+    {
+        var strings = line.Split(' ').ToArray();
+        return (input: strings[0], springLengths: strings[1].Split(',').Select(int.Parse).ToArray());
+    }
+
+    public static long CalculateLegalPositions(string input, IEnumerable<int> springLengths)
+    {
+        IReadOnlyList<char> chars = ("." + input + ".").ToCharArray();
+        IReadOnlyList<bool> springs = DamagedList(springLengths).ToArray();
+
+        long[,] table = new long[chars.Count + 1, springs.Count + 1];
+        table[table.GetUpperBound(0), table.GetUpperBound(1)] = 1;
+
+        for (int c = chars.Count - 1; c >= 0; c--)
+        {
+            for (int s = springs.Count - 1; s >= 0; s--)
+            {
+                table[c, s] = (chars[c] != '.', chars[c] != '#', springs[s]) switch
+                {
+                    (true, _, true) => table[c + 1, s + 1],
+                    (_, true, false) => table[c + 1, s + 1] + table[c + 1, s],
+                    _ => 0
+                };
+            }
+        }
+
+        return table[0, 0];
+    }
+
+    private static IEnumerable<bool> DamagedList(IEnumerable<int> springLengths){
+        yield return false;
+
+        foreach (var length in springLengths)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                yield return true;
+            }
+
+            yield return false;
+        }
     }
 }
--- tests/AdventOfCode.Tests/Year2023/Day12Tests.cs
+++ tests/AdventOfCode.Tests/Year2023/Day12Tests.cs
@@ -8,7 +8,12 @@ public class Day12Tests : IClassFixture<Day12>
 
     private const string TestData =
         """
-
+        ???.### 1,1,3
+        .??..??...?##. 1,1,3
+        ?#?#?#?#?#?#?#? 1,3,1,6
+        ????.#...#... 4,1,1
+        ????.######..#####. 1,6,5
+        ?###???????? 3,2,1
         """;
 
     public Day12Tests(Day12 sut)
@@ -17,16 +22,28 @@ public class Day12Tests : IClassFixture<Day12>
     }
 
     [Theory(DisplayName = "Part1 should return expected results from example data")]
-    [InlineData(TestData, -1)]
+    [InlineData(TestData, 21)]
     public void Part1Example(string input, int expected)
     {
-        _sut.Part1(input).As<int>().Should().Be(expected);
+        _sut.Part1(input).As<long>().Should().Be(expected);
     }
 
     [Theory(DisplayName = "Part2 should return expected results from example data")]
-    [InlineData(TestData, -1)]
+    [InlineData(TestData, 525152)]
     public void Part2Example(string input, int expected)
     {
-        _sut.Part2(input).As<int>().Should().Be(expected);
+        _sut.Part2(input).As<long>().Should().Be(expected);
+    }
+
+    [Theory(DisplayName = "CalculateLegalPositions should return expected results from example data")]
+    [InlineData("???.###", new[] {1, 1, 3}, 1)]
+    [InlineData(".??..??...?##.", new[] {1, 1, 3}, 4)]
+    [InlineData("?#?#?#?#?#?#?#?", new[] {1, 3, 1, 6}, 1)]
+    [InlineData("????.#...#...", new[] {4, 1, 1}, 1)]
+    [InlineData("????.######..#####.", new[] {1, 6, 5}, 4)]
+    [InlineData("?###????????", new[] {3, 2, 1}, 10)]
+    public void CalculateLegalPositionsExample(string input, int[] springLengths, long expected)
+    {
+        Day12.CalculateLegalPositions(input, springLengths).Should().Be(expected);
     }
 }
