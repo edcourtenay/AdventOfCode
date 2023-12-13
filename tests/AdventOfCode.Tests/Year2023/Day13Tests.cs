de30ed9af62e81cd026e2bbe9d43ec004023e793
Ed Courtenay <ed@edcourtenay.co.uk>
2023-12-13 13:42:33 +0000

    2023-13: Point of Incidence

---
+++ tests/AdventOfCode.Tests/Year2023/Day13Tests.cs
@@ -0,0 +1,56 @@
+using AdventOfCode.Year2023;
+
+namespace AdventOfCode.Tests.Year2023;
+
+public class Day13Tests : IClassFixture<Day13>
+{
+    private readonly Day13 _sut;
+
+    private const string Board1 =
+        """
+        #.##..##.
+        ..#.##.#.
+        ##......#
+        ##......#
+        ..#.##.#.
+        ..##..##.
+        #.#.##.#.
+        """;
+
+    private const string Board2 =
+        """
+        #...##..#
+        #....#..#
+        ..##..###
+        #####.##.
+        #####.##.
+        ..##..###
+        #....#..#
+        """;
+
+    private const string TestData =
+        $"""
+        {Board1}
+        
+        {Board2}
+        """;
+
+    public Day13Tests(Day13 sut)
+    {
+        _sut = sut;
+    }
+
+    [Theory(DisplayName = "Part1 should return expected results from example data")]
+    [InlineData(TestData, 405)]
+    public void Part1Example(string input, int expected)
+    {
+        _sut.Part1(input).As<int>().Should().Be(expected);
+    }
+
+    [Theory(DisplayName = "Part2 should return expected results from example data")]
+    [InlineData(TestData, 400)]
+    public void Part2Example(string input, int expected)
+    {
+        _sut.Part2(input).As<int>().Should().Be(expected);
+    }
+}
