abf1cd3de3cc4ba504b5d1304fc28f0dac150da2
Ed Courtenay <ed@edcourtenay.co.uk>
2023-12-15 22:15:12 +0000

    2022-16: Proboscidea Volcanium

---
+++ tests/AdventOfCode.Tests/Year2022/Day16Tests.cs
@@ -0,0 +1,41 @@
+﻿using AdventOfCode.Year2022;
+
+namespace AdventOfCode.Tests.Year2022;
+
+public class Day16Tests : IClassFixture<Day16>
+{
+    private readonly Day16 _sut;
+
+    private const string TestData =
+        """
+        Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+        Valve BB has flow rate=13; tunnels lead to valves CC, AA
+        Valve CC has flow rate=2; tunnels lead to valves DD, BB
+        Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+        Valve EE has flow rate=3; tunnels lead to valves FF, DD
+        Valve FF has flow rate=0; tunnels lead to valves EE, GG
+        Valve GG has flow rate=0; tunnels lead to valves FF, HH
+        Valve HH has flow rate=22; tunnel leads to valve GG
+        Valve II has flow rate=0; tunnels lead to valves AA, JJ
+        Valve JJ has flow rate=21; tunnel leads to valve II
+        """;
+
+    public Day16Tests(Day16 sut)
+    {
+        _sut = sut;
+    }
+
+    [Theory(DisplayName = "Part1 should return expected results from example data")]
+    [InlineData(TestData, 1651)]
+    public void Part1Example(string input, long expected)
+    {
+        _sut.Part1(input).As<long>().Should().Be(expected);
+    }
+
+    [Theory(DisplayName = "Part2 should return expected results from example data")]
+    [InlineData(TestData, 1707)]
+    public void Part2Example(string input, long expected)
+    {
+        _sut.Part2(input).As<long>().Should().Be(expected);
+    }
+}
