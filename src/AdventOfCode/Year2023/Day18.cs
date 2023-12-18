9c94f0539b860cde4de78a4c504325bcd8e30bb6
Ed Courtenay <ed@edcourtenay.co.uk>
2023-12-18 17:09:40 +0000

    2023-18: Lavaduct Lagoon

--- src/AdventOfCode/Year2023/Day18.cs
+++ src/AdventOfCode/Year2023/Day18.cs
@@ -1,15 +1,71 @@
+using System.Text.RegularExpressions;
+
+using Instruction = (char direction, int count);
+using Edge = ((long x, long y) from, (long x, long y) to);
+
 namespace AdventOfCode.Year2023;
 
-[Description("Not set...")]
-public class Day18 : IPuzzle
+[Description("Lavaduct Lagoon")]
+public sealed partial class Day18 : IPuzzle
 {
+    [GeneratedRegex(@"(?'direction'[UDLR])\s(?'count'\d+)\s\(#(?'colour'[0-9a-f]{6})\)", RegexOptions.Compiled)]
+    private static partial Regex LineRegex();
+
     public object Part1(string input)
     {
-        throw new NotImplementedException();
+        return Solve(input, match => (direction: match.Groups["direction"].Value[0], count: int.Parse(match.Groups["count"].Value)));
     }
 
     public object Part2(string input)
     {
-        throw new NotImplementedException();
+        return Solve(input, ColourToInstruction);
+    }
+
+    private static long Solve(string input, Func<Match, Instruction> matchToInstruction)
+    {
+        var edges = GetEdges(input.ToLines(line => LineRegex().Match(line))
+            .Where(match => match.Success)
+            .Select(matchToInstruction))
+            .ToList();
+
+        long perimeter = edges.Aggregate(0L, (acc, edge) => acc += Math.Abs(edge.to.x - edge.from.x) + Math.Abs(edge.to.y - edge.from.y), acc => acc / 2 + 1);
+        long area = edges.Aggregate(0L, (acc, edge) => acc += (edge.from.x + edge.to.x) * (edge.from.y - edge.to.y), acc => Math.Abs(acc / 2));
+
+        return area + perimeter;
+    }
+
+    private static Instruction ColourToInstruction(Match match)
+    {
+        int colour = Convert.ToInt32(match.Groups["colour"].Value, 16);
+        char direction = (colour & 0xF) switch
+        {
+            0x0 => 'R',
+            0x1 => 'U',
+            0x2 => 'L',
+            0x3 => 'D',
+            _ => throw new InvalidOperationException()
+        };
+        int count = (colour >> 4);
+
+        return (direction, count);
+    }
+
+    private static IEnumerable<Edge> GetEdges(IEnumerable<Instruction> data)
+    {
+        var from = (x: 0L, y: 0L);
+        foreach (var instruction in data)
+        {
+            var to = (instruction.direction) switch
+            {
+                'U' => (from.x, from.y - instruction.count),
+                'D' => (from.x, from.y + instruction.count),
+                'L' => (from.x - instruction.count, from.y),
+                'R' => (from.x + instruction.count, from.y),
+                _ => throw new InvalidOperationException()
+            };
+
+            yield return (from, to);
+            from = to;
+        }
     }
 }
--- tests/AdventOfCode.Tests/Year2023/Day18Tests.cs
+++ tests/AdventOfCode.Tests/Year2023/Day18Tests.cs
@@ -8,7 +8,20 @@ public class Day18Tests : IClassFixture<Day18>
 
     private const string TestData =
         """
-
+        R 6 (#70c710)
+        D 5 (#0dc571)
+        L 2 (#5713f0)
+        D 2 (#d2c081)
+        R 2 (#59c680)
+        D 2 (#411b91)
+        L 5 (#8ceee2)
+        U 2 (#caa173)
+        L 1 (#1b58a2)
+        U 2 (#caa171)
+        R 2 (#7807d2)
+        U 3 (#a77fa3)
+        L 2 (#015232)
+        U 2 (#7a21e3)
         """;
 
     public Day18Tests(Day18 sut)
@@ -17,16 +30,28 @@ public class Day18Tests : IClassFixture<Day18>
     }
 
     [Theory(DisplayName = "Part1 should return expected results from example data")]
-    [InlineData(TestData, -1)]
-    public void Part1Example(string input, int expected)
+    [InlineData(TestData, 62)]
+    public void Part1Example(string input, long expected)
     {
-        _sut.Part1(input).As<int>().Should().Be(expected);
+        _sut.Part1(input).As<long>().Should().Be(expected);
     }
 
     [Theory(DisplayName = "Part2 should return expected results from example data")]
-    [InlineData(TestData, -1)]
-    public void Part2Example(string input, int expected)
+    [InlineData(TestData, 952408144115)]
+    public void Part2Example(string input, long expected)
     {
-        _sut.Part2(input).As<int>().Should().Be(expected);
+        _sut.Part2(input).As<long>().Should().Be(expected);
+    }
+
+    [Fact(DisplayName = "Shoelace test")]
+    public void ShoelaceAreaTest()
+    {
+        ((int x, int y) from, (int x, int y) to)[] edges = {
+            ((0, 0), (4, 0)), ((4, 0), (4, 4)), ((4, 4), (0, 4)), ((0, 4), (0, 0))
+        };
+
+        var result = Math.Abs(edges.Aggregate(0, (acc, edge) => acc += (edge.from.x + edge.to.x) * (edge.from.y - edge.to.y)) / 2);
+
+        result.Should().Be(16);
     }
 }
