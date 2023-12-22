715a4275b77458e37f741fab0df0da895fcba3fd
Ed Courtenay <ed@edcourtenay.co.uk>
2023-12-22 20:26:27 +0000

    2023-22: Sand Slabs

--- tests/AdventOfCode.Tests/Year2023/Day22Tests.cs
+++ tests/AdventOfCode.Tests/Year2023/Day22Tests.cs
@@ -8,7 +8,21 @@ public class Day22Tests : IClassFixture<Day22>
 
     private const string TestData =
         """
+        1,0,1~1,2,1
+        0,0,2~2,0,2
+        0,2,3~2,2,3
+        0,0,4~0,2,4
+        2,0,5~2,2,5
+        0,1,6~2,1,6
+        1,1,8~1,1,9
+        """;
 
+    private const string TestData2 =
+        """
+        0,0,1~0,1,1
+        1,1,1~1,1,1
+        0,0,2~0,0,2
+        0,1,2~1,1,2
         """;
 
     public Day22Tests(Day22 sut)
@@ -17,14 +31,15 @@ public class Day22Tests : IClassFixture<Day22>
     }
 
     [Theory(DisplayName = "Part1 should return expected results from example data")]
-    [InlineData(TestData, -1)]
+    [InlineData(TestData, 5)]
+    [InlineData(TestData2, 3)]
     public void Part1Example(string input, int expected)
     {
         _sut.Part1(input).As<int>().Should().Be(expected);
     }
 
     [Theory(DisplayName = "Part2 should return expected results from example data")]
-    [InlineData(TestData, -1)]
+    [InlineData(TestData, 7)]
     public void Part2Example(string input, int expected)
     {
         _sut.Part2(input).As<int>().Should().Be(expected);
