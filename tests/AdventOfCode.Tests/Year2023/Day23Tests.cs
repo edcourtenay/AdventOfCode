e73c500d1ee0c03262a0c6571049e8e24cabcd54
Ed Courtenay <ed@edcourtenay.co.uk>
2023-12-23 23:13:55 +0000

    2023-23: A Long Walk

---
+++ tests/AdventOfCode.Tests/Year2023/Day23Tests.cs
@@ -0,0 +1,54 @@
+using AdventOfCode.Year2023;
+
+namespace AdventOfCode.Tests.Year2023;
+
+public class Day23Tests : IClassFixture<Day23>
+{
+    private readonly Day23 _sut;
+
+    private const string TestData =
+        """
+        #.#####################
+        #.......#########...###
+        #######.#########.#.###
+        ###.....#.>.>.###.#.###
+        ###v#####.#v#.###.#.###
+        ###.>...#.#.#.....#...#
+        ###v###.#.#.#########.#
+        ###...#.#.#.......#...#
+        #####.#.#.#######.#.###
+        #.....#.#.#.......#...#
+        #.#####.#.#.#########v#
+        #.#...#...#...###...>.#
+        #.#.#v#######v###.###v#
+        #...#.>.#...>.>.#.###.#
+        #####v#.#.###v#.#.###.#
+        #.....#...#...#.#.#...#
+        #.#########.###.#.#.###
+        #...###...#...#...#.###
+        ###.###.#.###v#####v###
+        #...#...#.#.>.>.#.>.###
+        #.###.###.#.###.#.#v###
+        #.....###...###...#...#
+        #####################.#
+        """;
+
+    public Day23Tests(Day23 sut)
+    {
+        _sut = sut;
+    }
+
+    [Theory(DisplayName = "Part1 should return expected results from example data")]
+    [InlineData(TestData, 94)]
+    public void Part1Example(string input, int expected)
+    {
+        _sut.Part1(input).As<int>().Should().Be(expected);
+    }
+
+    [Theory(DisplayName = "Part2 should return expected results from example data")]
+    [InlineData(TestData, 154)]
+    public void Part2Example(string input, int expected)
+    {
+        _sut.Part2(input).As<int>().Should().Be(expected);
+    }
+}
