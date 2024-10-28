46e190980bb03c2f77ff88e8fd96be5d554c0b20
Ed Courtenay <ed@edcourtenay.co.uk>
2024-10-28 23:30:13 +0000

    Template for Day 08

---
+++ tests/AdventOfCode.Tests/Year2024/Day08Tests.cs
@@ -0,0 +1,27 @@
+using AdventOfCode.Year2024;
+
+namespace AdventOfCode.Tests.Year2024;
+
+public class Day08Tests : IClassFixture<Day08>
+{
+    private readonly Day08 _sut;
+
+    private const string TestData = "";
+
+    public Day08Tests(Day08 sut)
+    {
+        _sut = sut;
+    }
+
+    [Fact]
+    public void Part1Example()
+    {
+        _sut.Part1(TestData).Should().Be("");
+    }
+    
+    [Fact]
+    public void Part2Example()
+    {
+        _sut.Part2(TestData).Should().Be("");
+    }
+}
