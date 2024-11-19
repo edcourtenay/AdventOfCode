08ffeb1f336caceeeb1384d9f662121bc7caaf7e
Ed Courtenay <ed@edcourtenay.co.uk>
2024-11-19 16:26:40 +0000

    Template for Day 25

---
+++ tests/AdventOfCode.Tests/Year2024/Day25Tests.cs
@@ -0,0 +1,27 @@
+using AdventOfCode.Solutions.Year2024;
+
+namespace AdventOfCode.Tests.Year2024;
+
+public class Day25Tests : IClassFixture<Day25>
+{
+    private readonly Day25 _sut;
+
+    private const string TestData = "";
+
+    public Day25Tests(Day25 sut)
+    {
+        _sut = sut;
+    }
+
+    [Fact]
+    public void Part1Example()
+    {
+        _sut.Part1(TestData).Should().Be("");
+    }
+    
+    [Fact]
+    public void Part2Example()
+    {
+        _sut.Part2(TestData).Should().Be("");
+    }
+}
