c11d1809817d0b27c596e737ca324904eb74ebc6
Ed Courtenay <ed@edcourtenay.co.uk>
2024-11-19 16:25:28 +0000

    Template for Day 15

---
+++ src/AdventOfCode.Solutions/Year2024/Day15.cs
@@ -0,0 +1,15 @@
+namespace AdventOfCode.Solutions.Year2024;
+
+[Description("Not Set")]
+public class Day15 : IPuzzle
+{
+    public object Part1(string input)
+    {
+        return string.Empty;
+    }
+
+    public object Part2(string input)
+    {
+        return string.Empty;
+    }
+}
