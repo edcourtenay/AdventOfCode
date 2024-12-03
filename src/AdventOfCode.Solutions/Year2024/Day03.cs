517561562a22bcf1a7fcedf7f64ddfe8d0ebf55d
Ed Courtenay <ed@edcourtenay.co.uk>
2024-12-03 09:18:46 +0000

    Completed

--- src/AdventOfCode.Solutions/Year2024/Day03.cs
+++ src/AdventOfCode.Solutions/Year2024/Day03.cs
@@ -1,15 +1,53 @@
+using System.Text.RegularExpressions;
+
 namespace AdventOfCode.Solutions.Year2024;
 
-[Description("Not Set")]
-public class Day03 : IPuzzle
+[Description("Mull It Over")]
+public partial class Day03 : IPuzzle
 {
     public object Part1(string input)
     {
-        return string.Empty;
+        var matches = Part1Regex().Matches(input);
+
+        int sum = 0;
+        foreach (Match match in matches)
+        {
+            sum += int.Parse(match.Groups["a"].Value) * int.Parse(match.Groups["b"].Value);
+        }
+
+        return sum;
     }
 
     public object Part2(string input)
     {
-        return string.Empty;
+        var matches = Part2Regex().Matches(input);
+
+        int sum = 0;
+        bool enabled = true;
+        foreach (Match match in matches)
+        {
+            switch (match.Groups["instruction"].Value)
+            {
+                case "do":
+                    enabled = true;
+                    break;
+                
+                case "don't":
+                    enabled = false;
+                    break;
+                
+                case "mul" when enabled:
+                    sum += int.Parse(match.Groups["a"].Value) * int.Parse(match.Groups["b"].Value);
+                    break;
+            }
+        }
+        
+        return sum;
     }
+
+    [GeneratedRegex(@"mul\((?'a'\d{1,3}),(?'b'\d{1,3})\)")]
+    private static partial Regex Part1Regex();
+
+    [GeneratedRegex(@"(?'instruction'mul)\((?'a'\d{1,3}),(?'b'\d{1,3})\)|(?'instruction'do)\(\)|(?'instruction'don\'t)\(\)")]
+    private static partial Regex Part2Regex();
 }
--- tests/AdventOfCode.Tests/Year2024/Day03Tests.cs
+++ tests/AdventOfCode.Tests/Year2024/Day03Tests.cs
@@ -6,7 +6,8 @@ public class Day03Tests : IClassFixture<Day03>
 {
     private readonly Day03 _sut;
 
-    private const string TestData = "";
+    private const string Part1TestData = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+    private const string Part2TestData = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
 
     public Day03Tests(Day03 sut)
     {
@@ -16,12 +17,12 @@ public class Day03Tests : IClassFixture<Day03>
     [Fact]
     public void Part1Example()
     {
-        _sut.Part1(TestData).Should().Be("");
+        _sut.Part1(Part1TestData).As<int>().Should().Be(161);
     }
     
     [Fact]
     public void Part2Example()
     {
-        _sut.Part2(TestData).Should().Be("");
+        _sut.Part2(Part2TestData).As<int>().Should().Be(48);
     }
 }
