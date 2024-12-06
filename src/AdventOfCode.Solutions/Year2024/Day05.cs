e361ad16a9bf1275d9b2c34bf26c211fc2ae6e4e
Ed Courtenay <ed@edcourtenay.co.uk>
2024-12-06 16:56:54 +0000

    2024-05: Print Queue

--- src/AdventOfCode.Solutions/Year2024/Day05.cs
+++ src/AdventOfCode.Solutions/Year2024/Day05.cs
@@ -1,15 +1,78 @@
 namespace AdventOfCode.Solutions.Year2024;
 
-[Description("Not Set")]
+[Description("Print Queue")]
 public class Day05 : IPuzzle
 {
     public object Part1(string input)
     {
-        return string.Empty;
+        var (comparer, pages) = Parse(input.ToLines());
+
+        return pages
+            .Where(x => SortedCorrectly(x, comparer))
+            .Sum(x => int.Parse(x[x.Length / 2]));
     }
 
     public object Part2(string input)
     {
-        return string.Empty;
+        var (comparer, pages) = Parse(input.ToLines());
+        
+        return pages
+            .Where(x => !SortedCorrectly(x, comparer))
+            .Sum(x => int.Parse(x.Order(comparer).ElementAt(x.Length / 2)));
+    }
+    
+    static (PageComparer comparer, List<string[]> pages) Parse(IEnumerable<string> lines)
+    {
+        HashSet<(string, string)> rules = [];
+        List<string[]> pages = [];
+        PageComparer comparer = new(rules);
+
+        using var enumerator = lines.GetEnumerator();
+        var blankSeen = false;
+        while (enumerator.MoveNext())
+        {
+            var line = enumerator.Current;
+
+            switch (line)
+            {
+                case "":
+                    blankSeen = true;
+                    break;
+                
+                case not null when !blankSeen:
+                    var split = line.Split('|');
+                    if (split is [{ } a, { } b])
+                    {
+                        rules.Add((a, b));
+                    }
+
+                    break;
+                    
+                case not null when blankSeen:
+                    pages.Add(line.Split(','));
+                    break;
+            }
+        }
+
+        return (comparer, pages);
+    }
+    
+    static bool SortedCorrectly(string[] page, PageComparer comparer)
+        => page.SequenceEqual(page.Order(comparer));
+    
+    class PageComparer(HashSet<(string, string)> rules) : IComparer<string>
+    {
+        public int Compare(string? x, string? y)
+        {
+            ArgumentNullException.ThrowIfNull(x);
+            ArgumentNullException.ThrowIfNull(y);
+
+            if (rules.Contains((x, y)))
+            {
+                return -1;
+            }
+
+            return rules.Contains((y, x)) ? 1 : 0;
+        }
     }
 }
--- tests/AdventOfCode.Tests/Year2024/Day05Tests.cs
+++ tests/AdventOfCode.Tests/Year2024/Day05Tests.cs
@@ -6,7 +6,37 @@ public class Day05Tests : IClassFixture<Day05>
 {
     private readonly Day05 _sut;
 
-    private const string TestData = "";
+    private const string TestData =
+        """
+        47|53
+        97|13
+        97|61
+        97|47
+        75|29
+        61|13
+        75|53
+        29|13
+        97|29
+        53|29
+        61|53
+        97|53
+        61|29
+        47|13
+        75|47
+        97|75
+        47|61
+        75|61
+        47|29
+        75|13
+        53|13
+
+        75,47,61,53,29
+        97,61,53,29,13
+        75,29,13
+        75,97,47,61,53
+        61,13,29
+        97,13,75,29,47
+        """;
 
     public Day05Tests(Day05 sut)
     {
@@ -16,12 +46,12 @@ public class Day05Tests : IClassFixture<Day05>
     [Fact]
     public void Part1Example()
     {
-        _sut.Part1(TestData).Should().Be("");
+        _sut.Part1(TestData).As<int>().Should().Be(143);
     }
     
     [Fact]
     public void Part2Example()
     {
-        _sut.Part2(TestData).Should().Be("");
+        _sut.Part2(TestData).As<int>().Should().Be(123);
     }
 }
