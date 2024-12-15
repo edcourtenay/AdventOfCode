453b33bc648866edec62c3453bbe2e5bd0d1931d
Ed Courtenay <ed@edcourtenay.co.uk>
2024-12-15 12:56:33 +0000

    WIP

--- src/AdventOfCode.Solutions/Year2024/Day07.cs
+++ src/AdventOfCode.Solutions/Year2024/Day07.cs
@@ -5,11 +5,51 @@ public class Day07 : IPuzzle
 {
     public object Part1(string input)
     {
-        return string.Empty;
+        var lines = input.ToLines(ParseLine);
+
+        return lines.Where(EvaluateLine).Sum(l => l.target);
     }
 
     public object Part2(string input)
     {
         return string.Empty;
+    }
+
+    public (long target, long[] numbers) ParseLine(string line)
+    {
+        string[] strings = line.Split([':', ' '], StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+
+        return (long.Parse(strings[0]), [..strings[1..].Select(long.Parse)]);
+    }
+
+    public bool EvaluateLine((long target, long[] numbers) line)
+    {
+        int combinations = (line.numbers.Length - 1) * 2;
+        for (int i = 0; i < combinations; i++)
+        {
+            int x = i;
+            long result = line.numbers[0];
+            for (int j = 1; j < line.numbers.Length; j++)
+            {
+                switch (x & 0x1)
+                {
+                    case 0x0:
+                        result += line.numbers[j];
+                        break;
+                    default:
+                        result *= line.numbers[j];
+                        break;
+                }
+
+                x >>= 1;
+            }
+
+            if (result == line.target)
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
--- tests/AdventOfCode.Tests/Year2024/Day07Tests.cs
+++ tests/AdventOfCode.Tests/Year2024/Day07Tests.cs
@@ -6,7 +6,18 @@ public class Day07Tests : IClassFixture<Day07>
 {
     private readonly Day07 _sut;
 
-    private const string TestData = "";
+    private const string TestData =
+        """
+        190: 10 19
+        3267: 81 40 27
+        83: 17 5
+        156: 15 6
+        7290: 6 8 6 15
+        161011: 16 10 13
+        192: 17 8 14
+        21037: 9 7 18 13
+        292: 11 6 16 20
+        """;
 
     public Day07Tests(Day07 sut)
     {
@@ -16,12 +27,21 @@ public class Day07Tests : IClassFixture<Day07>
     [Fact]
     public void Part1Example()
     {
-        _sut.Part1(TestData).Should().Be("");
+        _sut.Part1(TestData).As<int>().Should().Be(3749);
     }
     
     [Fact]
     public void Part2Example()
     {
         _sut.Part2(TestData).Should().Be("");
+    }
+
+    [Theory]
+    [InlineData("190: 10 19", true)]
+    [InlineData("3267: 81 40 27", true)]
+    public void EvaluateLineTests(string lineData, bool expected)
+    {
+        (long target, long[] numbers) parsedLine = _sut.ParseLine(lineData);
+        _sut.EvaluateLine(parsedLine).Should().Be(expected);
     }
 }
